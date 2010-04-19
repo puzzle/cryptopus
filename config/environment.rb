8644e1a523e7be032ceb1af7e37351f48a1ac335
Andreas Zuber <zuber@puzzle.ch>
2010-04-19 13:40:31 +0200

    updated to rails version 2.3.5

--- config/environment.rb
+++ config/environment.rb
@@ -7,7 +7,7 @@
 HTTPS_HOST = 'http://localhost:8080/'
 
 # Specifies gem version of Rails to use when vendor/rails is not present
-RAILS_GEM_VERSION = '2.3.2' unless defined? RAILS_GEM_VERSION
+RAILS_GEM_VERSION = '2.3.5' unless defined? RAILS_GEM_VERSION
 
 # Bootstrap the Rails environment, frameworks, and default configuration
 require File.join(File.dirname(__FILE__), 'boot')
