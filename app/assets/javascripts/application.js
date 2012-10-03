85a23fb520a16617d78f0c63bfa582b09fd994c9
amaierhofer <a.maierhofer@gmail.com>
2012-10-03 15:30:20 +0200

    cleanup js code

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,10 +1,6 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
 //= require prototype
-//= require effects
-//= require controls
-//= require rails
-//= require dragdrop
 
 var auto_logoff_time = 10800;
 var remaining_seconds = auto_logoff_time+1;
