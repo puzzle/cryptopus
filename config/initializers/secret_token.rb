e2bbee55eff0378e1c9e88f68cd6472922cd500e
amaierhofer <a.maierhofer@gmail.com>
2012-10-03 14:46:00 +0200

    running on 3.2.8

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Cryptopus::Application.config.secret_token = 'bd3dcb477da4c9b7430e14b575ebf9e936d34b6db840c9fb3bc5e11fd788eb97defcb59bd35c6d9b6b63991568cb133000864246142d42fbf40ec5101486c0e5'
