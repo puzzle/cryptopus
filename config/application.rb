2e90528dab6bc3e3d6a4af0164a983020c2a00a6
amaierhofer <a.maierhofer@gmail.com>
2012-10-03 15:28:32 +0200

    move key to initializes, update js

--- config/application.rb
+++ config/application.rb
@@ -12,9 +12,5 @@ module Cryptopus
     config.filter_parameters += [:password, :private_key]
     config.assets.enabled = true
     config.time_zone = 'UTC'
-    config.action_controller.session = {
-      :key => '_cryptopus_session',
-      :secret      => 'c7b70e637f082ea9f091fea2728c5850fdb6a33f683a9513a10693eb9a0eb4ef6a6b08a2657e7b31c3b512db8bfaaa367e91600dfd1d6f9ece2667c5d8d84a58'
-    }
   end
 end
