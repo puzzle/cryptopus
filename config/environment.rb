1eaa645e68f68ae7710aff7377d9b8e2647f105a
Andreas Zuber <zuber@puzzle.ch>
2013-02-04 15:43:02 +0100

    rails 3.2.11 update

--- config/environment.rb
+++ config/environment.rb
@@ -2,7 +2,4 @@
 require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
-require 'benchmark'
-puts "initialize app: #{Benchmark.measure { Cryptopus::Application.initialize! } }"
-
-HTTPS_HOST = 'http://localhost:8080/'
+Cryptopus::Application.initialize!
