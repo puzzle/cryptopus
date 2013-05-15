9f87dc31aee79e4ba10a647576e1c291f044e292
Thomas Rawyler <rawyler@gmail.com>
2013-05-15 16:28:12 +0200

    added expiration time to session and displaying remaining time until auto-logout

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -5,7 +5,7 @@
 //= require jquery
 //= require jquery_ujs
 
-var auto_logoff_time = 10800;
+var auto_logoff_time = 300;
 var remaining_seconds = auto_logoff_time+1;
 
 function auto_logoff() {
@@ -15,8 +15,23 @@ function auto_logoff() {
   }
 
   remaining_seconds -= 1;
-  $('countdown').update(remaining_seconds); 
+  $('#countdown').html(humanize(remaining_seconds)); 
   setTimeout("auto_logoff()",1000);
 }
+
+/**
+ * Make seconds human readable.
+ */
+function humanize(seconds){
+  if(seconds > 60) {
+  	return Math.ceil(seconds / 60) + "m";
+  } else {
+  	return seconds + "s"
+  }
+}
+
+$(document).ready(function(){
+  auto_logoff();
+});
 
 // Event.observe(window, 'load', auto_logoff, false);
