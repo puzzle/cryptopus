4a08ca50a051a48c25859869fb8b6dd4dfa78b7c
pascal simon <ps@s-it.ch>
2015-01-27 21:45:46 +0100

    add some more unit tests for CryptUtils

--- spec/lib/crypt_utils_spec.rb
+++ spec/lib/crypt_utils_spec.rb
@@ -4,6 +4,10 @@ describe CryptUtils do
 
   before(:each) do
     @password = 'foo password'
+    @team_password = CryptUtils.new_team_password
+    keypair = CryptUtils.new_keypair
+    @private_key = CryptUtils.get_private_key_from_keypair(keypair)
+    @public_key = CryptUtils.get_public_key_from_keypair(keypair) 
   end
 
   describe "#one_way_crypt" do
@@ -13,5 +17,31 @@ describe CryptUtils do
     end
   end
 
+  describe "team password" do
+    it "should enrcypt and decrypt team password" do
+      encrypted_password = CryptUtils.encrypt_team_password(@team_password, @public_key)
+      decrypted_password = CryptUtils.decrypt_team_password(encrypted_password, @private_key)
+      expect(decrypted_password).to eq(@team_password)
+    end
+  end
+
+  describe "private key" do
+    it "should encrypt and decrypt private key" do
+      encrypted_private_key = CryptUtils.encrypt_private_key(@private_key, @password)
+      decrypted_private_key = CryptUtils.decrypt_private_key(encrypted_private_key, @password)
+      expect(decrypted_private_key).to eq (@private_key)
+    end
+  end
+
+  describe "key pair" do
+    it "should not raise exception if valid keypair" do
+      CryptUtils.validate_keypair(@private_key, @public_key)
+    end
+
+    it "should raise expection if keypair does not match" do
+      public_key = CryptUtils.get_public_key_from_keypair(CryptUtils.new_keypair)
+      expect { CryptUtils.validate_keypair(@private_key, public_key) }.to raise_error(Exceptions::DecryptFailed)
+    end
+  end
 
 end
