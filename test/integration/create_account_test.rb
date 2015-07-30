c74155e569fb332fcd9c7cc3b11a9084e8b711eb
Pascal Simon <simon@puzzle.ch>
2015-07-30 10:04:05 +0200

    create account test: use fixture way for finding entities

--- test/integration/create_account_test.rb
+++ test/integration/create_account_test.rb
@@ -4,13 +4,14 @@ class CreateAccountTest < ActionDispatch::IntegrationTest
 
   test 'bob creates new account' do
     login_as('bob')
-    team = Team.find_by_name('team1')
-    group = Group.find_by_name('group1')
+
+    team = teams(:team1)
+    group = groups(:group1)
     accounts_path = team_group_accounts_path(team_id: team.id, group_id: group.id)
 
     post_via_redirect accounts_path, account: {accountname: 'e-mail', username: 'bob@test', password: 'alice33'}
 
-    account = Account.find_by_accountname('e-mail')
+    account = group.accounts.find_by_accountname('e-mail')
     assert account
 
     get team_group_account_path(team_id: team.id, group_id: group.id, id: account.id)
@@ -21,11 +22,10 @@ class CreateAccountTest < ActionDispatch::IntegrationTest
 
   test 'alice reads account data' do
     login_as('alice')
-    team = Team.find_by_name('team1')
-    group = Group.find_by_name('group1')
-    account = Account.find_by_accountname('account1')
 
-    assert account
+    team = teams(:team1)
+    group = groups(:group1)
+    account = accounts(:account1)
 
     get team_group_account_path(team_id: team.id, group_id: group.id, id: account.id)
 
