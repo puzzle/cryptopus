235f14c7255fc4a5d86fa72aaf5a3fb6dd8d7529
Janiss Binder <binder@puzzle.ch>
2015-08-04 11:01:01 +0200

    Revised after review

--- test/support/integration_test/account_team_setup_integration_test_helper.rb
+++ test/support/integration_test/account_team_setup_integration_test_helper.rb
@@ -4,19 +4,19 @@ module IntegrationTest
       login_as(username.to_s, user_password)
 
       #New Team
-      post teams_path, team: {name: 'teamname', description: 'team_description', private: private, noroot: noroot}
+      post teams_path, team: {name: 'Web', description: 'team_description', private: private, noroot: noroot}
 
       #New Group
-      team_id = Team.find_by_name('teamname').id
-      post team_groups_path(team_id: team_id), group: {name: 'groupname', description: 'group_description'}
+      team = Team.find_by_name('Web')
+      post team_groups_path(team_id: team.id), group: {name: 'Default', description: 'group_description'}
 
       #New Account
-      group_id = Group.find_by_name('groupname').id
-      account_path = team_group_accounts_path(team_id: team_id, group_id: group_id)
-      post account_path, account: {accountname: 'accountname', description: 'account_description', username: 'account_username', password: 'account_password'}
+      group = team.groups.find_by_name('Default')
+      account_path = team_group_accounts_path(team_id: team.id, group_id: group.id)
+      post account_path, account: {accountname: 'puzzle', description: 'account_description', username: 'account_username', password: 'account_password'}
 
       logout
-      account = Account.find_by_accountname('accountname')
+      group.accounts.find_by_accountname('puzzle')
     end
 
     def create_team_group_account_noroot(username, user_password = 'password')
