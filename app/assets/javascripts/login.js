1bcfea1d8d5161c2f34a98ad9f845273a9b34f27
Janiss Binder <binder@puzzle.ch>
2015-08-05 13:40:24 +0200

    Save username for login

---
+++ app/assets/javascripts/login.js
@@ -0,0 +1,22 @@
+var password_input = $('#password');
+var user_input = $('#username');
+
+$(document).ready(function() {
+  var stored_username = localStorage.getItem('username');
+
+  if (stored_username != null && stored_username != ""){
+    user_input.val(stored_username);
+    password_input.focus();
+  } else {
+    user_input.focus();
+  }
+});
+
+$('#loginform').submit(function createLocalStorageEntry() {
+  var input_username = user_input.val();
+  localStorage.setItem('username', input_username);
+});
+
+user_input.click(function() {
+  user_input.val('');
+});
