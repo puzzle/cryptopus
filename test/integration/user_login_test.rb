2981431c0918e92f1221d436c4a8652ea6c4bb88
Janiss Binder <binder@puzzle.ch>
2015-08-06 09:11:13 +0200

    added integration logout test

--- test/integration/user_login_test.rb
+++ test/integration/user_login_test.rb
@@ -3,12 +3,18 @@ class UserLoginTest < ActionDispatch::IntegrationTest
 include IntegrationTest::DefaultHelper
   test 'bob logs in' do
     login_as('bob')
-    assert_equal(request.fullpath, teams_path)
+    assert_equal teams_path, request.fullpath
   end
 
   test 'bob logs in with wrong password' do
     login_as('bob', 'wrong_password')
     assert_includes flash[:error], 'Authentication failed'
-    assert_equal request.fullpath, '/login/authenticate'
+    assert_equal authenticate_login_path, request.fullpath
+  end
+
+  test 'bob logs out manuel' do
+    login_as('bob')
+    get_via_redirect logout_login_path
+    assert_equal login_login_path, request.fullpath
   end
 end
