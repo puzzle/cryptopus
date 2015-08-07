685b2e37f9c63610975297370acd2878da802032
Janiss Binder <binder@puzzle.ch>
2015-08-07 09:16:18 +0200

    First solution of add attachment to account

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -40,36 +40,36 @@ public
   # POST /teams/1/groups/1/accounts/1/items
   def create
     datafile = params[:item][:file]
-
     @item = @account.items.new
     @item.description = params[:item][:description]
     @item.filename = datafile.original_filename
     @item.content_type = datafile.content_type
-    @item.file = CryptUtils.encrypt_blob( datafile.read, get_team_password )
+    @item.file = CryptUtils.encrypt_blob( datafile.read, get_team_password(@team) )
     @item.created_on = Time.now
     @item.updated_on = Time.now
-      
+
     respond_to do |format|
       if @item.save
         flash[:notice] = t('flashes.items.uploaded')
       end
       format.html { redirect_to team_group_account_url(@team, @group, @account) }
     end
+    session[:jumpto][:item].delete(:file)
   end
-  
+
   # POST /teams/1/groups/1/accounts/1/items/1
   def show
     @item = @account.items.find( params[:id] )
-    file = CryptUtils.decrypt_blob( @item.file, get_team_password )
-    
+    file = CryptUtils.decrypt_blob( @item.file, get_team_password(@team) )
+
     send_data file, :filename => @item.filename , :type => @item.content_type, :disposition => 'attachment'
   end
-  
+
   # DELETE /teams/1/groups/1/accounts/1/items/1
   def destroy
     @item = @account.items.find( params[:id] )
     @item.destroy
-      
+
     respond_to do |format|
       format.html { redirect_to team_group_account_url(@team, @group, @account) }
     end
