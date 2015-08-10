4c2c122ad9b17c3aa8a249f409cb8f8291299095
Janiss Binder <binder@puzzle.ch>
2015-08-10 16:02:17 +0200

    created attachment test

---
+++ test/integration/add_attachment_to_account_test.rb
@@ -0,0 +1,29 @@
+require 'test_helper'
+class CreateAccountTest < ActionDispatch::IntegrationTest
+  include IntegrationTest::DefaultHelper
+  test 'add and remove attachment to account1' do
+    team = teams(:team1)
+    group = groups(:group1)
+    account = accounts(:account1)
+
+    login_as('bob')
+    items_path = team_group_account_items_path team, group, account
+    file_path = 'test/fixtures/files/teams/team1/account1/test_file.txt'
+    post items_path, item: {file: fixture_file_upload(file_path, 'text/plain')}
+    logout
+
+    login_as('alice')
+    file = account.items.first
+    item_path = team_group_account_item_path team, group, account, file
+    get item_path
+    assert_equal 'test', response.body
+    assert_equal 'text/plain', response.header['Content-Type']
+    assert_include response.header['Content-Disposition'], 'test_file.txt'
+    delete item_path
+    logout
+
+    login_as('bob')
+    error = assert_raises(ActiveRecord::RecordNotFound) { get item_path }
+    assert_includes error.message, "Couldn't find Item"
+  end
+end
