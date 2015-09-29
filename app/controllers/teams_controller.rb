1bee77efcc5ef3ca181a34679818196f00c28088
Janiss Binder <binder@puzzle.ch>
2015-09-29 11:27:27 +0200

    changed from params[:team] to team_params in teams_controller

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -21,71 +21,6 @@ class TeamsController < ApplicationController
 
   before_filter :validate_change_rights, :only => [:edit, :update, :destroy]
 
-private
-  def team_params
-    params.require(:team).permit(:name, :private, :noroot, :description)
-  end
-  
-  def validate_change_rights
-    unless am_i_team_member( params[:id] )
-      redirect_to :controller => 'login', :action => 'noaccess', :message => "You are not member of this team"
-      return
-    end
-  end
-
-  def add_user_to_team( user, admin )
-    team_member = user.teammembers.new
-    team_member.team_id = @team.id
-    team_member.password = CryptUtils.encrypt_team_password( @team_password, user.public_key )
-    if admin == true
-      team_member.admin = true
-    end
-    team_member.save
-  end
-
-  def add_root_to_team
-    root = User.find_by_uid( "0" )
-
-    # Check if the root is already in the Team
-    teammember_root = @team.teammembers.find_by_user_id( root.id )
-    return unless teammember_root.nil?
-
-    add_user_to_team( root, true )
-  end
-
-  def add_admins_to_team
-    admins = User.where(admin:  true ).load
-    for admin in admins do
-      # Exclude root
-      next if admin.uid == 0
-
-      # Check it the Admin is already in the Team
-      already_in_team = false
-      teammembers_admin = @team.teammembers.where(user_id: admin.id ).load
-      for teammember_admin in teammembers_admin do
-        already_in_team = true if teammember_admin.admin == true
-      end
-      next if already_in_team == true
-
-      add_user_to_team( admin, true )
-    end
-  end
-
-  def remove_root_from_team
-    root = User.find_by_uid( "0" )
-    teammember_root = @team.teammembers.find_by_user_id( root.id )
-    teammember_root.destroy unless teammember_root.nil?
-  end
-
-  def remove_admins_from_team
-    admins = @team.teammembers.where(admin: true)
-    for admin in admins do
-      admin.destroy unless admin.user.root?
-    end
-  end
-
-public
-
   # GET /teams
   def index
     if session[:user_id].nil?
@@ -149,7 +84,7 @@ public
     @team.updated_on = Time.now
 
     respond_to do |format|
-      if @team.update_attributes( params[:team] )
+      if @team.update_attributes( team_params )
 
         @team_password = get_team_password(@team)
 
@@ -183,8 +118,66 @@ public
     end
   end
 
-  def team_params
-    params.require(:team).permit(:name, :description, :private, :noroot)
-  end
+  private
+    def team_params
+      params.require(:team).permit(:name, :private, :noroot, :description)
+    end
 
+    def validate_change_rights
+      unless am_i_team_member( params[:id] )
+        redirect_to :controller => 'login', :action => 'noaccess', :message => "You are not member of this team"
+        return
+      end
+    end
+
+    def add_user_to_team( user, admin )
+      team_member = user.teammembers.new
+      team_member.team_id = @team.id
+      team_member.password = CryptUtils.encrypt_team_password( @team_password, user.public_key )
+      if admin == true
+        team_member.admin = true
+      end
+      team_member.save
+    end
+
+    def add_root_to_team
+      root = User.find_by_uid( "0" )
+
+      # Check if the root is already in the Team
+      teammember_root = @team.teammembers.find_by_user_id( root.id )
+      return unless teammember_root.nil?
+
+      add_user_to_team( root, true )
+    end
+
+    def add_admins_to_team
+      admins = User.where(admin:  true ).load
+      for admin in admins do
+        # Exclude root
+        next if admin.uid == 0
+
+        # Check it the Admin is already in the Team
+        already_in_team = false
+        teammembers_admin = @team.teammembers.where(user_id: admin.id ).load
+        for teammember_admin in teammembers_admin do
+          already_in_team = true if teammember_admin.admin == true
+        end
+        next if already_in_team == true
+
+        add_user_to_team( admin, true )
+      end
+    end
+
+    def remove_root_from_team
+      root = User.find_by_uid( "0" )
+      teammember_root = @team.teammembers.find_by_user_id( root.id )
+      teammember_root.destroy unless teammember_root.nil?
+    end
+
+    def remove_admins_from_team
+      admins = @team.teammembers.where(admin: true)
+      for admin in admins do
+        admin.destroy unless admin.user.root?
+      end
+    end
 end
