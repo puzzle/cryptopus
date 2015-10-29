d42a0d70641426e4244f8f472e16b18675f9120f
Janiss Binder <binder@puzzle.ch>
2015-10-29 15:16:33 +0100

    added validation presence true for :username

--- app/models/user.rb
+++ app/models/user.rb
@@ -17,6 +17,7 @@
 
 class User < ActiveRecord::Base
   validates :username, uniqueness: true
+  validates :username, presence: true
 
   has_many :teammembers, :dependent => :destroy
   has_many :recryptrequests, :dependent => :destroy
