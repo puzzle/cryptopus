054d02942124bdb90463eb385f44249beddf18a5
Janiss Binder <binder@puzzle.ch>
2015-11-23 14:49:03 +0100

    fixing routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,48 +1,44 @@
 Cryptopus::Application.routes.draw do
-  resources :recryptrequests
-  resources :teams do
-    resources :teammembers
-    resources :groups do
-      resources :accounts do
-        resources :items
+  scope "(:locale)", locale: /en|de|fr/ do
+    resources :recryptrequests
+    resources :teams do
+      resources :teammembers
+      resources :groups do
+        resources :accounts do
+          resources :items
+        end
       end
     end
-  end
 
-  namespace :admin do
-    resource :settings do
-      post 'update_all'
-      get 'index'
-    end
-
-    resources :users do
-      post 'unlock'
-    end
-    resources :recryptrequests do
-      collection do
-        post 'resetpassword'
+    namespace :admin do
+      resource :settings do
+        post 'update_all'
+        get 'index'
+      end
+      resources :users
+      resources :recryptrequests do
+        collection do
+          post 'resetpassword'
+        end
       end
     end
+
+    resource :login do
+      get 'login'
+      get 'show_update_password'
+      post 'update_password'
+      get 'logout'
+      get 'noaccess'
+      post 'authenticate'
+      post 'changelocale'
+    end
+
+    get 'wizard', to: 'wizard#index'
+    post 'wizard/apply'
+
+    get 'search', to: 'search#index'
+    get 'search/account', to: 'search#account'
+
+    root to: 'search#index'
   end
-
-  resource :login do
-    get 'login'
-    get 'show_update_password'
-    post 'update_password'
-    get 'logout'
-    get 'noaccess'
-    post 'authenticate'
-    post 'changelocale'
-  end
-
-
-
-  get 'wizard', to: 'wizard#index'
-  post 'wizard/apply'
-
-  get 'search', to: 'search#index'
-  get 'search/account', to: 'search#account'
-
-  root to: 'search#index'
 end
-
