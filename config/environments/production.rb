8056ac23fae63e85aad3a518f18bc45a05214241
pascal simon <ps@s-it.ch>
2015-12-04 14:57:11 +0100

    fix assets and assets precompilation

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -46,7 +46,7 @@ Cryptopus::Application.configure do
   # config.action_controller.asset_host = "http://assets.example.com"
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
-  # config.assets.precompile += %w( search.js )
+  config.assets.precompile += %w( login.js auto_logoff.js )
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
@@ -67,4 +67,6 @@ Cryptopus::Application.configure do
 
 
   config.eager_load = true
+
+  config.log_level = :info
 end
