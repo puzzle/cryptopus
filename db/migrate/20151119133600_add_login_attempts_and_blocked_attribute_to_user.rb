64674c8c0382840039d1816d90299aa9549b57a5
pascal simon <ps@s-it.ch>
2015-12-04 15:10:56 +0100

    fix anti brute-force user migration

--- db/migrate/20151119133600_add_login_attempts_and_blocked_attribute_to_user.rb
+++ db/migrate/20151119133600_add_login_attempts_and_blocked_attribute_to_user.rb
@@ -1,15 +1,7 @@
 class AddLoginAttemptsAndBlockedAttributeToUser < ActiveRecord::Migration
-  class << self
-    def up
-      add_column :users, :locked, :boolean, default: false
-      add_column :users, :last_failed_login_attempt_at, :datetime
-      add_column :users, :failed_login_attempts, :integer, default: 0, null: false
-    end
-
-    def down
-      remove_column :users, :locked
-      remove_column :users, :last_failed_login_attempt_at
-      remove_column :users, :failed_login_attempts
-    end
+  def change
+    add_column :users, :locked, :boolean, default: false
+    add_column :users, :last_failed_login_attempt_at, :datetime
+    add_column :users, :failed_login_attempts, :integer, default: 0, null: false
   end
 end
