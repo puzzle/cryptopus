c7c1bc41ce1151f93f1f9e19e020517858e06459
Janiss Binder <binder@puzzle.ch>
2016-04-29 13:54:49 +0200

    updated licenses

--- app/controllers/admin/logs_controller.rb
+++ app/controllers/admin/logs_controller.rb
@@ -1,3 +1,10 @@
+# encoding: utf-8
+
+#  Copyright (c) 2008-2016, Puzzle ITC GmbH. This file is part of
+#  Cryptopus and licensed under the Affero General Public License version 3 or later.
+#  See the COPYING file at the top-level directory or at
+#  https://github.com/puzzle/cryptopus.
+
 class Admin::LogsController < Admin::AdminController
   before_action :set_log, only: [:show, :edit, :update, :destroy]
 
--- app/models/log.rb
+++ app/models/log.rb
@@ -1,3 +1,10 @@
+# encoding: utf-8
+
+#  Copyright (c) 2008-2016, Puzzle ITC GmbH. This file is part of
+#  Cryptopus and licensed under the Affero General Public License version 3 or later.
+#  See the COPYING file at the top-level directory or at
+#  https://github.com/puzzle/cryptopus.
+
 class Log < ActiveRecord::Base
   default_scope {order('created_at DESC')}
   validates_inclusion_of :log_type, in: %w(maintenance_task)
