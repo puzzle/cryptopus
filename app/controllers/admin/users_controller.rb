f4971b035d8689a635ba892e30afa2350e1de3f5
Janiss Binder <binder@puzzle.ch>
2016-05-30 12:36:23 +0200

    simplify method

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -64,8 +64,8 @@ class Admin::UsersController < Admin::AdminController
   # DELETE /admin/users/1/destroy_soloteams
   def destroy_with_soloteams
     teams = teams_to_delete(user)
-    teams.each do |t| t.destroy end
-    destroy()
+    teams.destroy_all
+    destroy
   end
 
   # GET /admin/users/new
