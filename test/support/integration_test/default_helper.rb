43c2d5096cb05efc81aae2b84380649341220da5
Patrick Wenger <wenger@puzzle.ch>
2016-06-27 11:42:41 +0200

    created new story, fixed build for refs #18302

--- test/support/integration_test/default_helper.rb
+++ test/support/integration_test/default_helper.rb
@@ -33,11 +33,8 @@ module IntegrationTest
     def cannot_access_account(account_path, username, user_password = 'password')
       login_as(username, user_password)
       get account_path
-
-
       assert_match /You are not member of this team/, flash[:error]
       assert_redirected_to teams_path
-
       logout
     end
   end
