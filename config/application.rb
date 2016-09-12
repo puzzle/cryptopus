51663ab226ac66083758ebf521594afdc6088253
Janiss Binder <binder@puzzle.ch>
2016-09-12 11:35:38 +0200

    fixed jenkins job

--- config/application.rb
+++ config/application.rb
@@ -18,7 +18,6 @@ end
 
 module Cryptopus
   class Application < Rails::Application
-    config.force_ssl = true
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
