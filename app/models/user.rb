0c6858e171ef8ba236dbd0ada2c2788b21ba43de
Felix Ammann <ammann@puzzle.ch>
2016-10-10 14:24:46 +0200

    validates after review #20854

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,6 +10,7 @@ class User < ActiveRecord::Base
 
   validates :username, uniqueness: true
   validates :username, presence: true
+  validates :username, length: { maximum: 20 } 
 
   has_many :teammembers, dependent: :destroy
   has_many :recryptrequests, dependent: :destroy
