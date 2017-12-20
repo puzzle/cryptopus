bf43ac959b6f9dba5d7cf16a4edc66fe9735047a
Dominik Fischli <fischli@puzzle.ch>
2017-12-20 14:50:19 +0100

    Improved readability and style

---
+++ app/utils/changelog_version.rb
@@ -0,0 +1,24 @@
+# Copyright (c) 2008-2017, Puzzle ITC GmbH. This file is part of
+# Cryptopus and licensed under the Affero General Public License version 3 or later.
+# See the COPYING file at the top-level directory or at
+# https://github.com/puzzle/cryptopus.
+
+class ChangelogVersion
+  attr_accessor :major_version, :minor_version, :log_entries, :version
+
+  def initialize(header_line)
+    values         = header_line.split('.')
+    @version       = header_line
+    @major_version = values.first.to_i
+    @minor_version = values.second.casecmp('x').zero? ? Float::INFINITY : values.second.to_i
+    @log_entries   = []
+  end
+
+  def <=>(other)
+    [major_version, minor_version] <=> [other.major_version, other.minor_version]
+  end
+
+  def label
+    "Version #{version}"
+  end
+end
---
+++ test/unit/utils/changelog_reader_test.rb
@@ -0,0 +1,85 @@
+#  encoding: utf-8
+
+#  Copyright (c) 2008-2017, Puzzle ITC GmbH. This file is part of
+#  Cryptopus and licensed under the Affero General Public License version 3 or later.
+#  See the COPYING file at the top-level directory or at
+#  https://github.com/puzzle/cryptopus.
+
+require 'test_helper'
+class ChangelogReaderTest < ActiveSupport::TestCase
+
+  context 'verify CHANGELOG.md' do
+
+    test 'Every changelog entry has a version' do
+      ChangelogReader.changelog.each do |changelog_entry|
+        version = changelog_entry.version
+        assert_not_empty(version)
+      end
+    end
+
+    test 'Every changlog entry has at least one entry' do
+      ChangelogReader.changelog.each do |changelog_entry|
+        assert_not_empty(changelog_entry.log_entries)
+      end
+    end
+  end
+
+  context 'test ChangelogReader' do
+
+    subject { ChangelogReader.new }    
+
+    let(:changelog_lines) do
+      [
+        'foo',  #Invalid line
+        '## Version 1.3',
+        'fee', #Invalid line
+        '* change',
+        '* another change',
+        '## Version 1.1',
+        '* changes'
+      ].join("\n")
+    end
+
+    before do
+      subject.instance_variable_set(:@changelogs, [])
+    end
+
+    test 'reads log correctly' do      
+      subject.send(:parse_changelog_lines, changelog_lines)
+
+
+      changelogs = subject.instance_variable_get(:@changelogs)
+
+      assert_equal(2, changelogs.count)
+
+      version13 = changelogs[0]
+      assert_equal(2, version13.log_entries.count)
+      assert_equal('1.3', version13.version)
+      assert_equal('change', version13.log_entries[0])
+      assert_equal('another change', version13.log_entries[1])
+
+      version11 = changelogs[1]
+      assert_equal(1, version11.log_entries.count)
+      assert_equal('1.1', version11.version)
+      assert_equal('changes', version11.log_entries[0])
+    end
+
+    test 'header line parsed' do
+      version = subject.send(:changelog_header_line, '## Version 0.0')
+      assert_equal('0.0', version)
+    end
+
+    test 'entry line parsed' do
+      entry = subject.send(:changelog_entry_line, '* change')
+      assert_equal('change', entry)
+    end
+
+    test 'doesnt parse invalid line' do
+      version = subject.send(:changelog_header_line, 'invalid')
+      assert_nil(version)
+
+      entry = subject.send(:changelog_entry_line, 'invalid')
+      assert_nil(entry)
+    end
+  end
+end
