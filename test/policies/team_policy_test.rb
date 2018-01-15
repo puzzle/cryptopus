337c8fdea4e05ae1eccdac87efd6604de04720cc
Dominik Fischli <fischli@puzzle.ch>
2018-01-15 17:40:15 +0100

    cleaned teampolicy

--- test/policies/team_policy_test.rb
+++ test/policies/team_policy_test.rb
@@ -1,19 +1,49 @@
 require 'test_helper'
 
-class TeamPolicyTest < ActiveSupport::TestCase
+class TeamPolicyTest < PolicyTest
+  context '#index' do
+    test 'admin sees all non-private teams' do
 
-  def test_scope
+    end
+
+    test 'user can see all teams he is a part of' do
+
+    end
+
+    test 'user cannot see teams he isnt a part of' do
+
+    end
   end
 
-  def test_show
+  context '#update' do
+    test 'teammember can edit a team' do
+      
+    end
+
+    test 'non-teammember cannot edit a team' do
+
+    end
+
+    test 'admin can edit a public team' do
+
+    end
+
+    test 'admin cannot edit a non-public team' do
+
+    end
   end
 
-  def test_create
-  end
+  context '#destroy' do
+    test 'non-teammember cannot delete a team'do
 
-  def test_update
-  end
+    end
 
-  def test_destroy
+    test 'teammember can delete a team' do
+
+    end
+
+    test 'admin can delete any team' do
+
+    end
   end
 end
