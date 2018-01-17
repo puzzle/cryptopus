6117024a81b8176134ff1bcc9fe9f7edba56f1ae
Miro Zurbuchen <zurbuchen@puzzle.ch>
2018-01-17 17:13:21 +0100

    wip team

--- test/policies/team_policy_test.rb
+++ test/policies/team_policy_test.rb
@@ -1,31 +1,19 @@
 require 'test_helper'
 
 class TeamPolicyTest < PolicyTest
-  context '#index' do
-    test 'admin sees all non-private teams' do
 
-    end
-
-    test 'user can see all teams he is a part of' do
-
-    end
-
-    test 'user cannot see teams he isnt a part of' do
-
-    end
-  end
 
   context '#update' do
     test 'teammember can edit a team' do
-      
+      assert_permit bob, team2, :edit?
     end
 
     test 'non-teammember cannot edit a team' do
-
+      refute_permit alice, team2, :edit?
     end
 
     test 'admin can edit a public team' do
-
+      assert_permit admin, team2, :edit?
     end
 
     test 'admin cannot edit a non-public team' do
@@ -45,5 +33,28 @@ class TeamPolicyTest < PolicyTest
     test 'admin can delete any team' do
 
     end
+  end
+
+#  context '#scope' do
+#    test 'admin sees all non-private teams' do
+#      assert_not_nil policy_scope(admin, Team)
+#    end
+#
+#    test 'user can see all teams he is a part of' do
+#      assert_not_nil policy_scope(bob, Team)
+#    end
+#
+#    test 'user cannot see teams he isnt a part of' do
+#
+#    end
+#  end
+  #
+  private
+
+  def team2
+    teams(:team2)
+  end
+
+  def non_public_team
   end
 end
