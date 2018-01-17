0382e78cce54f68f1c6f69fc736ea787d6ee5573
Miro Zurbuchen <zurbuchen@puzzle.ch>
2018-01-17 16:41:14 +0100

    item policy

---
+++ app/policies/item_policy.rb
@@ -0,0 +1,23 @@
+class ItemPolicy < TeamChildrenPolicy
+
+  def initialize(current_user, item)
+    @current_user = current_user
+    @item = item
+  end
+
+  def show?
+    team_member?(@current_user, @item.account.group.team)
+  end
+
+  def new?
+    team_member?(@current_user, @item.account.group.team)
+  end
+
+  def create?
+    team_member?(@current_user, @item.account.group.team)
+  end
+
+  def destroy?
+    team_member?(@current_user, @item.account.group.team)
+  end
+end
