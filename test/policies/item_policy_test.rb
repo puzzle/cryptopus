34e03ec5646f2e369586c240a0212ad8d74e572f
Miro Zurbuchen <zurbuchen@puzzle.ch>
2018-01-31 16:59:36 +0100

    tested item policy

--- test/policies/item_policy_test.rb
+++ test/policies/item_policy_test.rb
@@ -1,34 +1,27 @@
 require 'test_helper'
 
 class ItemPolicyTest < PolicyTest
+
+  before(:each) do
+    remove_alice_from_team
+  end
+
   context '#show' do
     test 'teammember can show item' do
-      Item.any_instance.expects(:account)
-                       .returns(account)
-
       assert_permit bob, item1, :show?
     end
 
     test 'non teammember cannot show item' do
-      Item.any_instance.expects(:account)
-                       .returns(account)
-
       refute_permit alice, item1, :show?
     end
   end
 
   context '#new' do
     test 'teammember can create a new item' do
-      Item.any_instance.expects(:account)
-                       .returns(account)
-
       assert_permit bob, item1, :new?
     end
 
     test 'non teammember cannot create a new item' do
-      Item.any_instance.expects(:account)
-                       .returns(account)
-
       refute_permit alice, item1, :new?
     end
   end
@@ -45,24 +38,24 @@ class ItemPolicyTest < PolicyTest
   
   context '#destroy' do
     test 'teammember can destroy item' do
-      Item.any_instance.expects(:account)
-                       .returns(account)
-
       assert_permit bob, item1, :destroy?
     end
 
     test 'non teammember cannot destroy item' do
-      Item.any_instance.expects(:account)
-                       .returns(account)
-
       refute_permit alice, item1, :destroy?
     end
   end
 
   private
+
+  def remove_alice_from_team
+    team1 = Team.find_by(name: 'team1')
+    teammember = team1.teammembers.find_by(id: 2367674)
+    teammember.destroy!
+  end
   
   def account
-    Account.find_by(id: 185855917)
+    Account.find_by(accountname: 'account2')
   end
 
   def item1
