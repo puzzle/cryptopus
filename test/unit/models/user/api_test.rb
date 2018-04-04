2c664e2285a3a77634227b81e2087c1d24f694f5
Pascal Simon <simon@puzzle.ch>
2018-04-04 07:45:25 +0200

    api user locked?: code review clean-up

--- test/unit/models/user/api_test.rb
+++ test/unit/models/user/api_test.rb
@@ -12,95 +12,103 @@ class User::ApiTest < ActiveSupport::TestCase
   context '#create' do
 
     test 'creates new token for human user' do
-      @api_user = bob.api_users.create!(description: 'firefox plugin')
+      api_user = bob.api_users.create!(description: 'firefox plugin')
 
-      token = decrypted_token
+      token = decrypted_token(api_user)
       assert_match(/\A[a-z0-9]{32}\z/, token)
-      assert_equal(true, @api_user.authenticate(token))
-      assert_match(/\Abob-[a-z0-9]{6}\z/, @api_user.username)
-      assert_equal(60, @api_user.valid_for)
+      assert_equal(true, api_user.authenticate(token))
+      assert_match(/\Abob-[a-z0-9]{6}\z/, api_user.username)
+      assert_equal(60, api_user.valid_for)
     end
 
     test 'creation fails if no human user assigned' do
-      @api_user = User::Api.create
+      api_user = User::Api.create
 
-      assert_equal(false, @api_user.persisted?)
-      assert_match(/can't be blank/, @api_user.errors.messages[:username].first)
-      assert_match(/can't be blank/, @api_user.errors.messages[:human_user].first)
+      assert_equal(false, api_user.persisted?)
+      assert_match(/can't be blank/, api_user.errors.messages[:username].first)
+      assert_match(/can't be blank/, api_user.errors.messages[:human_user].first)
     end
 
-    test 'creation fails for illegal options' do
-      option_failure(42)
-      option_failure(-20)
-      option_failure(-1)
+    test 'invalid value for valid_for option' do
+      api_user = bob.api_users.new(description: 'api-access')
+
+      [42 -20 -1].each do |v|
+        api_user.valid_for = v
+        assert_equal(false, api_user.valid?)
+        assert_match(/is not included in the list/, api_user.errors.messages[:valid_for].first)
+      end
     end
 
-    test 'creation passes for all valid options' do
-      option_success 1.minute.seconds
-      option_success 5.minute.seconds
-      option_success 12.hours.seconds
-      option_success 0
+    test 'valid value for valid_for option' do
+      api_user = bob.api_users.new(description: 'api-access')
+
+      [1.minute.seconds, 5.minute.seconds,
+       12.hours.seconds, 0].each do |v|
+        api_user.valid_for = v
+        assert_equal true, api_user.valid?
+        assert_equal v, api_user.valid_for
+      end
     end
   end
 
   context '#renew' do
 
     test 'creates new token and updates expiring time' do
-      @now = Time.now
-      @api_user = bob.api_users.create
-      @api_user.options.valid_for = 5.minutes.seconds
-      @api_user.options.valid_until = @now
-      token = @api_user.password
+      now = Time.now
+      api_user = bob.api_users.create
+      api_user.options.valid_for = 5.minutes.seconds
+      api_user.options.valid_until = now
+      token = api_user.password
 
-      Time.expects(:now).at_least_once().returns(@now)
+      Time.expects(:now).at_least_once().returns(now)
 
-      @api_user.renew_token(bob.decrypt_private_key('password'))
+      api_user.renew_token(bob.decrypt_private_key('password'))
 
-      assert_equal @now.advance(seconds: 5.minutes.seconds).to_i, @api_user.valid_until.to_i
-      assert_not_equal token, @api_user.password
+      assert_equal now.advance(seconds: 5.minutes.seconds).to_i, api_user.valid_until.to_i
+      assert_not_equal token, api_user.password
     end
 
     test 'fails with the wrong userpassword' do
-      @now = Time.now
-      @api_user = bob.api_users.create
-      @api_user.options.valid_for = 5.minutes.seconds
-      @api_user.options.valid_until = @now
-      token = @api_user.password
+      now = Time.now
+      api_user = bob.api_users.create
+      api_user.options.valid_for = 5.minutes.seconds
+      api_user.options.valid_until = now
+      token = api_user.password
 
       assert_raise Exceptions::AuthenticationFailed do
-        @api_user.renew_token(SecureRandom.hex(16))
+        api_user.renew_token(SecureRandom.hex(16))
       end
 
-      assert_equal @now.to_i, @api_user.valid_until.to_i
-      assert_equal token, @api_user.password
+      assert_equal now.to_i, api_user.valid_until.to_i
+      assert_equal token, api_user.password
     end
   end
 
   context '#locked' do
     test 'user not locked' do
-      @api_user = bob.api_users.create
+      api_user = bob.api_users.create
 
       assert_equal false, bob.locked?
-      assert_equal false, @api_user.locked?
+      assert_equal false, api_user.locked?
     end
 
     test 'api user locked' do
-      @api_user = bob.api_users.create
+      api_user = bob.api_users.create
 
-      @api_user.update_attribute(:locked, true)
+      api_user.update_attribute(:locked, true)
       
       assert_equal false, bob.locked?
-      assert_equal true, @api_user.locked?
+      assert_equal true, api_user.locked?
     end
 
-    test 'user locked' do
-      @api_user = bob.api_users.create
+    test 'human user locked' do
+      api_user = bob.api_users.create
 
       bob.update_attribute(:locked, true)
-      @api_user.reload
+      api_user.reload
 
       assert_equal true, bob.locked?
-      assert_equal true, @api_user.locked?
+      assert_equal true, api_user.locked?
     end
   end
 
@@ -108,26 +116,13 @@ class User::ApiTest < ActiveSupport::TestCase
   end
 
   private
-
-  def option_success(seconds)
-    @api_user = bob.api_users.new(valid_for: seconds, description: 'api-access')
-
-    assert_equal true, @api_user.valid?
-    assert_equal seconds, @api_user.valid_for
-  end
-
-  def option_failure(seconds)
-    @api_user = bob.api_users.new(valid_for: seconds, description: 'api-access')
-    assert_equal(false, @api_user.valid?)
-    assert_match(/is not included in the list/, @api_user.errors.messages[:valid_for].first)
-  end
   
   def bob
     users(:bob)
   end
 
-  def decrypted_token
-    @api_user.send(:decrypt_token, bobs_private_key)
+  def decrypted_token(api_user)
+    api_user.send(:decrypt_token, bobs_private_key)
   end
 
   def bobs_private_key
