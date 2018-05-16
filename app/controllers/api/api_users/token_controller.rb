80bce7d6bcb0ef13f2fd3e592560f411a5c7fef6
Miro Zurbuchen <zurbuchen@puzzle.ch>
2018-05-16 15:05:34 +0200

    renamed api to api user

--- app/controllers/api/api_users/token_controller.rb
+++ app/controllers/api/api_users/token_controller.rb
@@ -9,23 +9,23 @@ class Api::ApiUsers::TokenController < ApiController
 
   # GET /api/api_users/token/1
   def show
-    authorize api
-    token = api.renew_token(session[:private_key])
-    add_info(t('flashes.api.api-users.token.renew', username: api.username, token: token))
-    render_json api
+    authorize api_user
+    token = api_user.renew_token(session[:private_key])
+    add_info(t('flashes.api.api-users.token.renew', username: api_user.username, token: token))
+    render_json api_user
   end
 
   # DELETE /api/api_users/token/1
   def destroy
-    authorize api
-    api.update!(locked: true)
-    add_info(t('flashes.api.api-users.token.invalidated', username: api.username))
+    authorize api_user
+    api_user.update!(locked: true)
+    add_info(t('flashes.api.api-users.token.invalidated', username: api_user.username))
     render_json ''
   end
 
   private
 
-  def api
-    @api ||= User::Api.find(params[:id])
+  def api_user
+    @api_user ||= User::Api.find(params[:id])
   end
 end
