fb3e5ccb883fc55ce2f4a57bac593ae93da569f1
Pascal Simon <simon@puzzle.ch>
2018-06-25 21:52:48 +0200

    do not configure airbrake in test, development env

--- config/initializers/errbit.rb
+++ config/initializers/errbit.rb
@@ -5,27 +5,41 @@
 #  See the COPYING file at the top-level directory or at
 #  https://github.com/puzzle/cryptopus.
 
-Airbrake.configure do |config|
-  # if no host is given, ignore all environments
-  config.environment = Rails.env
-  config.ignore_environments = [:development, :test]
-  config.ignore_environments << :production if ENV['RAILS_AIRBRAKE_HOST'].blank?
+if defined?(Airbrake)
 
-  config.project_id     = 1 # required, but any positive integer works
-  config.project_key    = ENV['RAILS_AIRBRAKE_API_KEY']
-  config.host           = ENV['RAILS_AIRBRAKE_HOST']
-  config.blacklist_keys << 'RAILS_DB_PASSWORD'
-  config.blacklist_keys << 'RAILS_AIRBRAKE_API_KEY'
-  config.blacklist_keys << 'RAILS_SECRET_TOKEN'
-  config.blacklist_keys << /password/i
-end
+  Airbrake.configure do |config|
+    # if no host is given, ignore all environments
+    config.environment = Rails.env
+    config.ignore_environments = [:development, :test]
+    config.ignore_environments << :production if ENV['RAILS_AIRBRAKE_HOST'].blank?
 
-ignored_exceptions = %w(ActionController::MethodNotAllowed
-                        ActionController::RoutingError
-                        ActionController::UnknownHttpMethod)
-
-Airbrake.add_filter do |notice|
-  if (notice[:errors].map { |e| e[:type] } & ignored_exceptions).present?
-    notice.ignore!
+    config.project_id     = 1 # required, but any positive integer works
+    config.project_key    = ENV['RAILS_AIRBRAKE_API_KEY']
+    config.host           = ENV['RAILS_AIRBRAKE_HOST']
+    config.blacklist_keys << 'RAILS_DB_PASSWORD'
+    config.blacklist_keys << 'RAILS_AIRBRAKE_API_KEY'
+    config.blacklist_keys << 'RAILS_SECRET_TOKEN'
+    config.blacklist_keys << /password/i
   end
-end
+
+  ignored_exceptions = %w(ActionController::MethodNotAllowed
+                          ActionController::RoutingError
+                          ActionController::UnknownHttpMethod)
+
+  Airbrake.add_filter do |notice|
+    if (notice[:errors].map { |e| e[:type] } & ignored_exceptions).present?
+      notice.ignore!
+    end
+  end
+
+else # defined?(Airbrake)
+
+  # Create stub module for development and test
+  module Airbrake
+    extend self
+
+    def notify(*args)
+    end
+  end
+
+end # defined?(Airbrake)
