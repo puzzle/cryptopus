871c3b7e6bca5b96361decef8f21a3072114e677
Miro Zurbuchen <zurbuchen@puzzle.ch>
2018-12-03 13:46:13 +0100

    implemented feedback

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -5,9 +5,9 @@
 
 class Admin::UsersController < ApplicationController
 
-  before_action :redirect_if_ldap_user, only: %i[update]
+  before_action :redirect_if_ldap_user, only: %i[update edit]
   before_action :authorize_user_class, only: %i[index new create]
-  before_action :authorize_user, only: %i[update unlock]
+  before_action :authorize_user, only: %i[update unlock edit]
 
   helper_method :update_role
 
