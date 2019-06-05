f01540eee922269bbe0fc537c6c6d31cd5e6a64b
Pascal Simon <simon@puzzle.ch>
2019-06-05 17:29:22 +0200

    remove pry statement

--- db/migrate/20190604054153_sqlite_boolean_values.rb
+++ db/migrate/20190604054153_sqlite_boolean_values.rb
@@ -3,7 +3,6 @@ class SqliteBooleanValues < ActiveRecord::Migration[5.2]
   # https://api.rubyonrails.org/classes/ActiveRecord/ConnectionAdapters/SQLite3Adapter.html#method-c-represent_boolean_as_integer
 
   def up
-    require 'pry'; binding.pry unless $pstop
     return unless sqlite?
 
     User.where("locked = 't'").update_all(locked: 1)
