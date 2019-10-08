009a381d8c25aef4c802f78c477145c70cc60192
Pascal Simon <simon@puzzle.ch>
2019-10-08 19:19:15 +0200

    sentry exclude mysql connection errors

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -1,5 +1,9 @@
 # frozen_string_literal: true
 
 Raven.configure do |config|
+
+  # exclude the following exceptions:
+  config.excluded_exceptions += %w[Mysql2::Error::ConnectionError]
+
   config.sanitize_fields = Rails.application.config.filter_parameters.map(&:to_s)
 end
