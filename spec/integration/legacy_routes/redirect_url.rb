15c1f47caa79f5cb2d5502c5b03c49764946a587
LBruegger <l.b2001@hotmail.ch>
2020-04-02 13:15:57 +0200

    fix rubocop

--- spec/integration/legacy_routes/redirect_url.rb
+++ spec/integration/legacy_routes/redirect_url.rb
@@ -5,7 +5,7 @@ require 'rails_helper'
 describe LegacyRoutes::RedirectUrl do
   include IntegrationTest::DefaultHelper
 
-  #/de/teams/1/groups/1 -> /teams/1/groups/1/
+  # /de/teams/1/groups/1 -> /teams/1/groups/1/
   it 'redirects to groups without groups in url' do
     legacy_account_url = "/de/teams/#{teams(:team1).id}/groups/#{groups(:group1).id}"
     login_as('bob')
@@ -15,7 +15,7 @@ describe LegacyRoutes::RedirectUrl do
     assert_redirected_to team_group_path(teams(:team1).id, groups(:group1).id)
   end
 
-  #/de/teams/1/groups/1/accounts -> /teams/1/groups/1/
+  # /de/teams/1/groups/1/accounts -> /teams/1/groups/1/
   it 'redirects to accounts url with accounts in legacy url' do
     legacy_account_url = "/de/teams/#{teams(:team1).id}/groups/#{groups(:group1).id}/accounts"
     login_as('bob')
@@ -25,7 +25,7 @@ describe LegacyRoutes::RedirectUrl do
     assert_redirected_to team_group_path(teams(:team1).id, groups(:group1).id)
   end
 
-  #/de/teams/1/groups -> /teams/1/
+  # /de/teams/1/groups -> /teams/1/
   it 'redirects to groups with groups in url' do
     legacy_team_url = "/de/teams/#{teams(:team1).id}/groups"
     login_as('bob')
@@ -35,7 +35,7 @@ describe LegacyRoutes::RedirectUrl do
     assert_redirected_to team_path(teams(:team1).id)
   end
 
-  #/de/teams/1/groups/1/ -> /teams/1/groups/1/
+  # /de/teams/1/groups/1/ -> /teams/1/groups/1/
   it 'redirects to teams url with locale in url' do
     legacy_team_url = "/de/teams/#{teams(:team1).id}/"
     redirect_url = team_path(teams(:team1).id)
@@ -46,9 +46,10 @@ describe LegacyRoutes::RedirectUrl do
     assert_redirected_to redirect_url
   end
 
-  #/de/teams/1/groups/1/accounts/1/ -> /accounts/1/
+  # /de/teams/1/groups/1/accounts/1/ -> /accounts/1/
   it 'redirects to team1 url without locale' do
-    legacy_account_url = "/de/teams/#{teams(:team1).id}/groups/#{groups(:group1).id}/accounts/#{accounts(:account1).id}/"
+    legacy_account_url = "/de/teams/#{teams(:team1).id}/groups/" \
+    "#{groups(:group1).id}/accounts/#{accounts(:account1).id}/"
     login_as('bob')
 
     get legacy_account_url
@@ -56,7 +57,7 @@ describe LegacyRoutes::RedirectUrl do
     assert_redirected_to account_path(accounts(:account1))
   end
 
-  #/de/teams' -> /teams
+  # /de/teams' -> /teams
   it 'redirects to teams url without locale' do
     login_as('bob')
 
@@ -65,14 +66,14 @@ describe LegacyRoutes::RedirectUrl do
     assert_redirected_to teams_path
   end
 
-  #/de/login/login -> /login/login
+  # /de/login/login -> /login/login
   it 'redirects to login url without locale' do
     get '/de/login/login'
 
     assert_redirected_to login_login_path
   end
 
-  #/de/teams/1/groups/1/accounts -> /accounts
+  # /de/teams/1/groups/1/accounts -> /accounts
   it 'redirects to account url without locale' do
     legacy_accounts_url = "/de/teams/#{teams(:team1).id}/groups/#{groups(:group1).id}/accounts"
     login_as('bob')
@@ -82,7 +83,7 @@ describe LegacyRoutes::RedirectUrl do
     assert_redirected_to team_group_path(teams(:team1), groups(:group1))
   end
 
-  #/de/profile -> /profile
+  # /de/profile -> /profile
   it 'redirects to profile url without locale' do
     login_as('bob')
     get '/de/profile'
@@ -90,7 +91,7 @@ describe LegacyRoutes::RedirectUrl do
     assert_redirected_to profile_path
   end
 
-  #/de/search?q= -> /search?q=
+  # /de/search?q= -> /search?q=
   it 'redirects to search url without locale' do
     login_as('bob')
     get '/de/search?q='
@@ -98,7 +99,7 @@ describe LegacyRoutes::RedirectUrl do
     assert_redirected_to search_path
   end
 
-  #/de/admin/settings/index -> /admin/settings/index
+  # /de/admin/settings/index -> /admin/settings/index
   it 'redirects to admin settings url without locale' do
     login_as('bob')
     get '/de/admin/settings/index'
