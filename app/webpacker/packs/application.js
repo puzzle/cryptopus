9666a0c58ecb479ab858fd961af25f31b4d67429
s.gilgen <gilgen@puzzle.ch>
2020-04-03 08:23:44 +0200

    Add images to webpacker

--- app/webpacker/packs/application.js
+++ app/webpacker/packs/application.js
@@ -12,7 +12,7 @@
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.
 //
-// const images = require.context('../images', true)
-// const imagePath = (name) => images(name, true)
+const images = require.context('../images', true)
+const imagePath = (name) => images(name, true)
 
-console.log('Hello World from Webpacker')
+// console.log('Hello World from Webpacker')
