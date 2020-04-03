c8f3359f14ebfd3922d3c7c4cc9028bae5c45cb9
s.gilgen <gilgen@puzzle.ch>
2020-04-03 14:08:34 +0200

    Remove js from webpacker, finish images

--- app/webpacker/packs/application.js
+++ app/webpacker/packs/application.js
@@ -9,7 +9,7 @@
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
-// them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
+// them with the image_tag helper in views (e.g <%= image_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.
 //
 const images = require.context('../images', true)
