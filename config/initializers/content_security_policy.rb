c34422a55c4f902ca54d31263d820bf90ba71969
LBruegger <l.b2001@hotmail.ch>
2020-04-16 13:24:26 +0200

    fix turbolinks

--- config/initializers/content_security_policy.rb
+++ config/initializers/content_security_policy.rb
@@ -9,7 +9,7 @@ Rails.application.config.content_security_policy do |policy|
   policy.font_src    :self, :https, :data
   policy.img_src     :self, :https, :data
   policy.object_src  :none
-  policy.script_src  "nonce-GWWv2NDGZRbuQ1njU/TL7Q==", "nonce-XfbIC/dbNMOTRmagGbVyXA==", :self
+  policy.script_src  :self, :https
   policy.style_src   :unsafe_inline, :self, :https
   # If you are using webpack-dev-server then specify webpack-dev-server host
   policy.connect_src :self, :https, "http://localhost:3035", "ws://localhost:3035" if Rails.env.development?
