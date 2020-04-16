6df29d087651fa69f8cfeb16e9df9073505c5fe7
LBruegger <l.b2001@hotmail.ch>
2020-04-16 08:30:33 +0200

    continue add tests for Roles

--- spec/controllers/admin/settings_controller_spec.rb
+++ spec/controllers/admin/settings_controller_spec.rb
@@ -6,8 +6,28 @@ describe Admin::SettingsController do
   include ControllerHelpers
 
   context 'POST update_all' do
-    it 'updates setting attributes' do
+    it 'updates setting attributes as admin' do
       login_as(:admin)
+      post :update_all, params: { setting: { general_country_source_whitelist: %w[CH UK],
+                                             general_ip_whitelist: ['192.168.1.1',
+                                                                    '192.168.1.2'] } }
+      expect(Setting.value(:general, :country_source_whitelist)).to eq(%w[CH UK])
+      expect(Setting.value(:general, :ip_whitelist)).to eq(['192.168.1.1', '192.168.1.2'])
+      expect(flash[:notice]).to match(/successfully updated/)
+    end
+
+    it 'updates setting attributes as conf_admin' do
+      login_as(:tux)
+      post :update_all, params: { setting: { general_country_source_whitelist: %w[CH UK],
+                                             general_ip_whitelist: ['192.168.1.1',
+                                                                    '192.168.1.2'] } }
+      expect(Setting.value(:general, :country_source_whitelist)).to eq(%w[CH UK])
+      expect(Setting.value(:general, :ip_whitelist)).to eq(['192.168.1.1', '192.168.1.2'])
+      expect(flash[:notice]).to match(/successfully updated/)
+    end
+
+    it 'updates setting attributes as root' do
+      login_as(:root)
       post :update_all, params: { setting: { general_country_source_whitelist: %w[CH UK],
                                              general_ip_whitelist: ['192.168.1.1',
                                                                     '192.168.1.2'] } }
