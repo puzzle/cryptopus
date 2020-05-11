0fa00b1c1dcaec156ad3808b2f4f9fd8c151d979
LBruegger <l.b2001@hotmail.ch>
2020-05-11 09:42:37 +0200

    review changes

--- frontend/app/routes/accounts/edit.js
+++ frontend/app/routes/accounts/edit.js
@@ -1,8 +1,8 @@
 import Route from '@ember/routing/route';
 
 export default Route.extend({
-  async model(params) {
-    return await this.store.findRecord('account', params.id)
+  model(params) {
+    return this.store.findRecord('account', params.id)
   }
 
 });
