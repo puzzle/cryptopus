08e193ace0721164d7a034920562cdb66b77316a
Luca Brügger <hello@lucabruegger.ch>
2020-05-20 13:52:33 +0200

    review changes

--- frontend/app/routes/accounts.js
+++ frontend/app/routes/accounts.js
@@ -1,4 +1,12 @@
 import Route from "@ember/routing/route";
+import { getOwner } from '@ember/application';
 
 export default class AccountsRoute extends Route {
+  get passwordStrength() {
+    return getOwner(this).lookup('service:password-strength');
+  }
+
+  beforeModel() {
+    return this.passwordStrength.load();
+  }
 }
