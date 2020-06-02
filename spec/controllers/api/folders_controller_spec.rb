ed07dc898db10c84d66047a5af9c37182f669a07
Lucas Habersaat <lucas@habersaat.ch>
2020-06-02 14:47:32 +0200

    wip controller specs

--- spec/controllers/api/folders_controller_spec.rb
+++ spec/controllers/api/folders_controller_spec.rb
@@ -5,6 +5,8 @@ require 'rails_helper'
 describe Api::FoldersController do
   include ControllerHelpers
 
+  let(:alice) { users(:alice) }
+
   context 'GET index' do
     it 'returns matching folders' do
 
@@ -52,4 +54,78 @@ describe Api::FoldersController do
       expect(folder_json['id']).to eq folder.id.to_s
     end
   end
+
+  context 'PUT update' do
+    it 'updates folder name and description with valid params structure' do
+      login_as(:alice)
+      folder = folders(:folder1)
+      team = teams(:team1)
+
+      update_params = { name: 'new_name', description: 'new_description' }
+      put :update, params: {team_id: team, id: folder, folder: update_params }
+
+      folder.reload
+
+      expect(folder.name).to eq 'new_name'
+      expect(folder.description).to eq 'new_description'
+
+    end
+
+    it 'does not update folder when user not teammember' do
+      request.headers['Authorization-User'] = alice.username
+      request.headers['Authorization-Password'] = Base64.encode64('password')
+
+      folder = folders(:folder2)
+
+      folder_params =
+          {
+              id: folder.id,
+              folder:
+                  {
+                      name: 'Folder Alice',
+                      description: 'yeah, i wanna steal that folder'
+                  }
+          }
+      patch :update, params: folder_params, xhr: true
+
+      folder.reload
+
+      expect(folder.name).to eq('team2')
+
+      expect(response).to have_http_status(403)
+    end
+
+  end
+
+  context 'POST create' do
+    it 'creates folder name and description' do
+      login_as(:alice)
+      team = teams(:team1)
+
+      new_folder_params = {
+          data: {
+              attributes: {
+                  name: 'Folder Alice',
+                  description: 'yeah'
+              },
+              relationships: {
+                  team: {
+                      data: {
+                          id: team.id,
+                          type: 'teams'
+                      }
+                  }
+              }
+          }
+      }
+
+      post :create, params: new_folder_params , xhr: true
+
+      folder.reload
+
+      expect(folder.name).to eq 'Folder Alice'
+      expect(folder.description).to eq 'yeah'
+    end
+  end
+
 end
