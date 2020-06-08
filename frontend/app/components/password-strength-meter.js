d53fc2b1d339a67c4d73c551ff9ba904a49c8687
Nils Rauch <nils.rauch@gmail.com>
2020-06-08 14:51:27 +0000

    Improve password meter, refs: #54325

--- frontend/app/components/password-strength-meter.js
+++ frontend/app/components/password-strength-meter.js
@@ -3,37 +3,36 @@ import Component from "@ember/component";
 import { tracked } from "@glimmer/tracking";
 import { isPresent } from "@ember/utils";
 
-
 export default class PasswordStrengthMeterComponent extends Component {
-
   @service passwordStrength;
 
+  @tracked barWidth = 0;
   @tracked score = 0;
 
   constructor() {
-    super(...arguments)
+    super(...arguments);
 
     this.passwordStrength.load();
   }
 
   didReceiveAttrs() {
-
-    if(isPresent(this.password)) {
+    if (isPresent(this.password)) {
       this.passwordStrength.strength(this.password).then(strength => {
-        let score = strength.score
+        this.score = strength.score;
 
-        if(score === 0)
-          this.score = 10;
-        else
-          this.score = score * 25;
+        this.barWidth = this.score === 0 ? 10 : this.score * 25;
       });
     } else {
       this.score = 0;
+      this.barWidth = 0;
     }
   }
 
   get progressClass() {
-    return "progress-bar-"+this.score;
+    return "progress-bar-" + this.barWidth;
   }
 
+  get scoreRanking() {
+    return ["", "weak", "fair", "good", "strong"][this.score];
+  }
 }
