e818e75d36f00c7b094b1fbf09ae62ea28ddccb6
Nils Rauch <rauch@puzzle.ch>
2020-06-17 14:14:32 +0200

    Add queryParams to teams route

--- frontend/app/routes/teams/index.js
+++ frontend/app/routes/teams/index.js
@@ -1,6 +1,18 @@
 import Route from "@ember/routing/route";
 
 export default class TeamsIndexRoute extends Route {
+  queryParams = {
+    team_ids: {
+      refreshModel: true
+    },
+    folder_ids: {
+      refreshModel: true
+    },
+    q: {
+      refreshModel: true
+    }
+  };
+
   model(params) {
     return this.store.query("team", params);
   }
