16110720043c17215c1731c2d99d8ec5fb0b6912
s.gilgen <gilgen@puzzle.ch>
2020-06-18 12:04:29 +0200

    Fix bug, imporve testing

--- spec/integration/user_provides_new_ldap_pw_spec.rb
+++ spec/integration/user_provides_new_ldap_pw_spec.rb
@@ -22,34 +22,32 @@ describe 'User provides new Ldap Pw' do
     it 'provides new ldap password and remembers old password' do
       ldap = double
 
-      expect(GeoIp).to receive(:activated?).at_least(:once).and_return(false)
-
       # Prepare for do
-      bob.update(auth: 'ldap')
-      bob.update(provider_uid: '42')
+      bob.update!(auth: 'ldap', provider_uid: '42')
 
       # Method call expectations
       expect(LdapConnection).to receive(:new).at_least(:once).and_return(ldap)
-      expect(ldap).to receive(:ldap_info).exactly(4).times
+      expect(ldap).to receive(:ldap_info).exactly(2).times
 
       expect(ldap).to receive(:authenticate!)
-        .with('bob', 'password')
-        .and_return(true)
-      expect(ldap).to receive(:authenticate!)
         .with('bob', 'newPassword')
-        .exactly(3).times
+        .exactly(4).times
         .and_return(true)
 
       # Calls login method
       # 2 times with login as and
       # 2 times in recryptrequests_controller
 
-      login_as('bob')
-
+      login_as('bob', 'newPassword')
+      expect(request.fullpath).to eq(recryptrequests_new_ldap_password_path)
       # Recrypt
       post recryptrequests_recrypt_path, params: { new_password: 'newPassword',
                                                    old_password: 'password' }
-      logout
+
+      follow_redirect!
+      expect(request.fullpath).to eq(session_destroy_path)
+      follow_redirect!
+      expect(request.fullpath).to eq(session_new_path)
 
       login_as('bob', 'newPassword')
 
@@ -63,17 +61,15 @@ describe 'User provides new Ldap Pw' do
     it 'provides new ldap password and doesnt remember his old password' do
       ldap = double
 
-      expect(GeoIp).to receive(:activated?).at_least(:once).and_return(false)
       # Prepare for  do
-      bob.update!(auth: 'ldap')
-      bob.update!(provider_uid: '42')
+      bob.update!(auth: 'ldap', provider_uid: '42')
 
       # Method call expectations
       expect(LdapConnection).to receive(:new).at_least(:once).and_return(ldap)
-      expect(ldap).to receive(:ldap_info).exactly(4).times
+      expect(ldap).to receive(:ldap_info).exactly(2).times
 
       expect(ldap).to receive(:authenticate!)
-        .with('bob', /newPassword|password/)
+        .with('bob', 'newPassword')
         .exactly(3).times
         .and_return(true)
 
@@ -81,13 +77,16 @@ describe 'User provides new Ldap Pw' do
       # 2 times with login as and
       # 1 time in recryptrequests_controller
 
-      login_as('bob')
-
+      login_as('bob', 'newPassword')
+      expect(request.fullpath).to eq(recryptrequests_new_ldap_password_path)
       # Recrypt
       post recryptrequests_recrypt_path, params: { forgot_password: true,
                                                    new_password: 'newPassword' }
 
-      get session_destroy_path
+      follow_redirect!
+      expect(request.fullpath).to eq(session_destroy_path)
+      follow_redirect!
+      expect(request.fullpath).to eq(session_new_path)
 
       login_as_root
       bobs_user_id = bob.id
@@ -109,15 +108,13 @@ describe 'User provides new Ldap Pw' do
       ldap = double
 
       # Prepare for  do
-      bob.update(auth: 'ldap')
-      bob.update(provider_uid: '42')
+      bob.update!(auth: 'ldap', provider_uid: '42')
 
       # Method call expectations
       expect(LdapConnection).to receive(:new).at_least(:once).and_return(ldap)
-      expect(ldap).to receive(:ldap_info).exactly(2).times
 
       expect(ldap).to receive(:authenticate!)
-        .with('bob', /newPassword|password/)
+        .with('bob', 'newPassword')
         .exactly(3).times
         .and_return(true)
 
@@ -125,11 +122,14 @@ describe 'User provides new Ldap Pw' do
       # 2 times with login as and
       # 1 time in recryptrequests_controller
 
-      login_as('bob')
-
+      login_as('bob', 'newPassword')
+      expect(request.fullpath).to eq(recryptrequests_new_ldap_password_path)
       # Recrypt
       post recryptrequests_recrypt_path, params: { new_password: 'newPassword',
                                                    old_password: 'wrong_password' }
+
+      follow_redirect!
+      expect(request.fullpath).to eq(recryptrequests_new_ldap_password_path)
 
       #  do if user got error messages
       expect(flash[:error]).to match(/Your OLD password was wrong/)
@@ -139,30 +139,31 @@ describe 'User provides new Ldap Pw' do
       ldap = double
 
       # Prepare for  do
-      bob.update(auth: 'ldap')
-      bob.update(provider_uid: '42')
+      bob.update!(auth: 'ldap', provider_uid: '42')
 
       # Method call expectations
       expect(LdapConnection).to receive(:new).at_least(:twice).and_return(ldap)
-      expect(ldap).to receive(:ldap_info).exactly(2).times
 
       expect(ldap).to receive(:authenticate!)
-        .with('bob', /newPassword|password/)
+        .with('bob', 'newPassword')
         .and_return(true)
 
       #  do if Bob can see his account (should not)
       # cannot_access_account(get_account_path, 'bob')
 
-      login_as('bob')
+      login_as('bob', 'newPassword')
 
-      # Recrypt
+      # Recryptrecryptrequests_recrypt_path
 
       expect(ldap).to receive(:authenticate!)
         .with('bob', 'wrong')
         .and_return(false)
 
-      post recryptrequests_recrypt_path, params: { new_password: 'wrong' }
+      expect(request.fullpath).to eq(recryptrequests_new_ldap_password_path)
 
+      post recryptrequests_recrypt_path, params: { new_password: 'wrong' }
+      follow_redirect!
+      expect(request.fullpath).to eq(recryptrequests_new_ldap_password_path)
       #  do if user got error messages
       expect(flash[:error]).to match(/Your NEW password was wrong/)
     end
@@ -171,21 +172,20 @@ describe 'User provides new Ldap Pw' do
       ldap = double
 
       # Prepare for  do
-      bob.update(auth: 'ldap')
-      bob.update(provider_uid: '42')
+      bob.update!(auth: 'ldap', provider_uid: '42')
 
       # Method call expectations
-      expect(LdapConnection).to receive(:new).exactly(3).times.and_return(ldap)
-      expect(ldap).to receive(:ldap_info).twice
+      expect(LdapConnection).to receive(:new).exactly(2).times.and_return(ldap)
 
       expect(ldap).to receive(:authenticate!)
-        .with('bob', 'password')
+        .with('bob', 'newPassword')
         .and_return(true)
 
       #  do if Bob can see his account (should not)
       # cannot_access_account(get_account_path, 'bob')
 
-      login_as('bob')
+      login_as('bob', 'newPassword')
+      expect(request.fullpath).to eq(recryptrequests_new_ldap_password_path)
 
       # Recrypt
       expect(ldap).to receive(:authenticate!)
