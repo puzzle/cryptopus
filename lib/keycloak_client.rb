f4fbc862530df025482b57464889110ce0989467
sgilgen <gilgen@puzzle.ch>
2020-06-24 11:24:16 +0200

    Rename keycloak attribute to include app name

--- lib/keycloak_client.rb
+++ lib/keycloak_client.rb
@@ -20,7 +20,7 @@ class KeycloakClient
     pk_secret_base = SecureRandom.base64(32)
     token = JSON.parse(Keycloak::Client.get_token_by_client_credentials)['access_token']
     user_attributes = JSON.parse(Keycloak::Admin.get_user(user_id, token))['attributes'] || {}
-    user_attributes['pk_secret_base'] = pk_secret_base
+    user_attributes['cryptopus_pk_secret_base'] = pk_secret_base
     Keycloak::Admin.update_user(user_id, { attributes: user_attributes }, token)
     pk_secret_base
   end
