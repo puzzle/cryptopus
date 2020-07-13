51a07653e1e5273c4f36a68dedd1e61b2214943b
Nils Rauch <rauch@puzzle.ch>
2020-07-13 16:24:07 +0200

    Handle error codes the api could deliver, refs: #54706

--- frontend/app/routes/application.js
+++ frontend/app/routes/application.js
@@ -1,7 +1,9 @@
 import Route from "@ember/routing/route";
 import { inject as service } from "@ember/service";
+import { action } from "@ember/object";
 
 export default class IndexRoute extends Route {
+  @service notify;
   @service intl;
   @service store;
 
@@ -11,5 +13,17 @@ export default class IndexRoute extends Route {
     /* eslint-enable no-undef */
     let selectedLocale = (localeDropdown[0] && localeDropdown[0].value) || "en";
     this.intl.setLocale(selectedLocale);
+  }
+
+  @action
+  error(error) {
+    if (error.message.includes("403") || error.message.includes("404")) {
+      let prefix = this.intl.locale[0].replace("-", "_");
+      let msg = this.intl.t(`${prefix}.${error.errors[0]}`);
+      this.notify.error(msg);
+      this.replaceWith("index");
+    } else if (error.message.includes("401")) {
+      window.location.replace("/session/new");
+    }
   }
 }
