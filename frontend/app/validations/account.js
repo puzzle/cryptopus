cbc28ec2a16391979a2c82472db8882bef9973c8
Luca Brügger <hello@lucabruegger.ch>
2020-07-16 16:10:47 +0200

    fix validations and autocomplete

--- frontend/app/validations/account.js
+++ frontend/app/validations/account.js
@@ -5,6 +5,8 @@ import {
 
 export default {
   accountname: [validatePresence(true), validateLength({ max: 70 })],
+  cleartextPassword: [validatePresence(true), validateLength({ min: 0, max: 70 })],
+  cleartextUsername: [validatePresence(true), validateLength({ min: 0, max: 70 })],
   description: [validateLength({ max: 4000 })],
   folder: [validatePresence(true)]
 };
