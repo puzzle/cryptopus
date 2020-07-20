aaabe1e091a30aac98726f823d7e98a4c78b994f
Lucas Habersaat <lucas@habersaat.ch>
2020-07-20 14:12:07 +0200

    Style welcome page

--- frontend/app/routes/index.js
+++ frontend/app/routes/index.js
@@ -1,10 +1,15 @@
 import BaseRoute from "./base";
 import { inject as service } from "@ember/service";
+import ENV from "../config/environment";
 
 export default class IndexRoute extends BaseRoute {
   @service navService;
 
   beforeModel() {
     this.navService.clear()
+  }
+
+  model() {
+    return ENV.currentUserGivenname;
   }
 }
