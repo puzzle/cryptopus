c89eee96a190c75386081c34b9f7954f9353c4c9
Luca Brügger <lbruegger@puzzle.ch>
2020-07-20 10:55:08 +0200

    fix e2e and validations

--- frontend/app/helpers/validation-error-key.js
+++ frontend/app/helpers/validation-error-key.js
@@ -1,9 +1,9 @@
-import { helper } from "@ember/component/helper";
-import { underscore } from "@ember/string";
+import {helper} from "@ember/component/helper";
+import {underscore} from "@ember/string";
 
 function validationErrorKey(args) {
   let error = args.flat()[0];
-  if (!error) return;
+  if (!error || !error.context) return;
 
   return `validations.${error.context.description.toLowerCase()}.${underscore(
     error.type
