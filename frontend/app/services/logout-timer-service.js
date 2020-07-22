ec23c9f53585db0f7f2b6247cc53d39f3dff2af1
Luca Brügger <lbruegger@puzzle.ch>
2020-07-22 12:59:26 +0200

    fix auto logoff

--- frontend/app/services/logout-timer-service.js
+++ frontend/app/services/logout-timer-service.js
@@ -27,7 +27,7 @@ export default class LogoutTimerService extends Service {
 
     this.logoutTimer.addEventListener('secondsUpdated', () => {
       let passedTime = this.logoutTimer.getTotalTimeValues().seconds
-      if (passedTime >= this.AUTOLOGOFF_TIME) {
+      if (passedTime === this.AUTOLOGOFF_TIME) {
         this.resetSession()
       } else {
         this.calculateTimeToLogoff(passedTime)
@@ -37,7 +37,7 @@ export default class LogoutTimerService extends Service {
 
   resetSession() {
     /* eslint-disable no-undef  */
-    fetch(`/session/destroy`, {
+    fetch(`/session/destroy/?autologout=true`, {
       method: "GET",
       headers: {
         "X-CSRF-Token": $('meta[name="csrf-token"]').attr("content")
