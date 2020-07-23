6c142f04643152615aa967c192feb0d3709933c1
Lucas Habersaat <lucas@habersaat.ch>
2020-07-23 14:51:35 +0200

    Rename account-entry to account-row

--- frontend/app/components/account-row.js
+++ frontend/app/components/account-row.js
@@ -3,7 +3,7 @@ import { action } from "@ember/object";
 import { tracked } from "@glimmer/tracking";
 import { inject as service } from "@ember/service";
 
-export default class AccountEntryComponent extends Component {
+export default class AccountRowComponent extends Component {
   @service store;
   @service router;
   @service intl;
