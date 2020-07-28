662712f65288af0773920203d03513e7b37b3d44
Lucas Habersaat <lucas@habersaat.ch>
2020-07-28 09:52:15 +0200

    wip

--- spec/integration/keycloak_user_login_spec.rb
+++ spec/integration/keycloak_user_login_spec.rb
@@ -24,23 +24,23 @@ describe 'Keycloak user login' do
       .with('asd', sso_url)
       .and_return('token')
     expect(Keycloak::Client).to receive(:get_attribute)
-      .with('sub')
+      .with('sub', 'asd')
       .at_least(:once)
       .and_return('asdQW123-asdQWE')
     expect(Keycloak::Client).to receive(:get_attribute)
-      .with('preferred_username')
+      .with('preferred_username', 'asd')
       .exactly(4).times
       .and_return('ben')
     expect(Keycloak::Client).to receive(:get_attribute)
-      .with('given_name')
+      .with('given_name', 'asd')
       .at_least(:once)
       .and_return('Ben')
     expect(Keycloak::Client).to receive(:get_attribute)
-      .with('family_name')
+      .with('family_name', 'asd')
       .at_least(:once)
       .and_return('Meier')
     expect(Keycloak::Client).to receive(:get_attribute)
-      .with('pk_secret_base')
+      .with('pk_secret_base', 'asd')
       .at_least(:once)
       .and_return(pk_secret_base)
     expect(Keycloak::Client).to receive(:user_signed_in?)
@@ -56,9 +56,46 @@ describe 'Keycloak user login' do
       follow_redirect!
       user = User.find_by(username: 'ben')
       expect(request.fullpath).to eq(root_path)
-      expect(response.body).to match(/Hi  Ben! Want to recover a password?/)
+      expect(response.body).to match(/Hi Ben! Looking for a password?/)
       expect(user.surname).to eq('Meier')
       expect(user.givenname).to eq('Ben')
     end.to change { User::Human.count }.by(1)
   end
+
+  it 'two logins', :threadsafety => true do
+    alice = login(:alice)
+    bob = login(:bob)
+    login_as(:bob)
+
+    alice.check_user('Alice')
+    bob.check_user('Bob')
+
+  end
+
+  private
+
+  module CustomDsl
+    def check_user(expected_givenname)
+      get api_env_settings_path
+      require 'pry'; binding.pry
+
+      givenname = JSON.parse(body)['current_user']['id']
+
+      expect(givenname).to be expected_givenname
+    end
+  end
+
+  def login(user)
+    open_session do |sess|
+      sess.extend(CustomDsl)
+      u = users(user)
+      sess.https!
+      sess.post "/session", params: { username: u.username, password: u.password }
+      require 'pry'; binding.pry
+      sess.follow_redirect!
+      assert_equal '/session', sess.path
+      sess.https!(false)
+    end
+  end
+
 end
