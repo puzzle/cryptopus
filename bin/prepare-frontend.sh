e0384386de806ba81ef3c7eeb1a6df67343af9ac
Nils Rauch <rauch@puzzle.ch>
2020-08-05 16:03:51 +0200

    Fix front and backend assets merge on build

--- bin/prepare-frontend.sh
+++ bin/prepare-frontend.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 yarn --cwd frontend/ install
 yarn --cwd frontend/ build-prod
-rm -r public/{assets,frontend-index.html}
-cp -r frontend/dist/* public/
+rm public/frontend-index.html
+rsync --ignore-existing -r frontend/dist/* public/
 mv public/{index,frontend-index}.html
