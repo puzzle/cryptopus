bdb5ec198b893c04da1c348da487f7832d238481
Nils Rauch <nils.rauch@gmail.com>
2020-09-23 15:36:19 +0000

    Refactor data encryption

--- app/models/account/encrypted_data.rb
+++ app/models/account/encrypted_data.rb
@@ -3,20 +3,35 @@
 class Account::EncryptedData
   include JsonSerializable
 
-  attr_accessor :data, :iv
+  attr_accessor :value, :iv, :cleartext_value
 
-  def initialize(data: nil, iv: nil)
-    @data = data
+  def initialize(value: nil, iv: nil)
+    @value = value
     @iv = iv
   end
 
-  def self.encrypt(encryption_key, data)
-    iv = CryptUtils.random_iv
-    new(data: CryptUtils.encrypt_base64(data, encryption_key, iv),
-        iv: Base64.strict_encode64(iv))
+  def encrypt(encryption_key)
+    value, iv = CryptUtils.encrypt_base64(cleartext_value.to_json, encryption_key)
+    self.value = value
+    self.iv = Base64.strict_encode64(iv)
   end
 
   def decrypt(encryption_key)
-    CryptUtils.decrypt_base64(data, encryption_key, Base64.strict_decode64(iv))
+    CryptUtils.decrypt_base64(value, encryption_key, Base64.strict_decode64(iv))
+  end
+
+  def to_json(*_args)
+    raise 'data not encrypted' if value_not_encrypted?
+
+    {
+      iv: iv,
+      value: value
+    }.to_json
+  end
+
+  private
+
+  def value_not_encrypted?
+    cleartext_value.present? && value.nil?
   end
 end
--- app/models/account/ose_secret.rb
+++ app/models/account/ose_secret.rb
@@ -11,21 +11,7 @@ class Account::OSESecret < Account
   end
 
   def encrypt(team_password)
-    self.encrypted_data = Account::EncryptedData.encrypt(team_password,
-                                                         { ose_secret: ose_secret }.to_json)
-  end
-
-  private
-
-  def decrypt_data(team_password)
-    return if encrypted_data.blank?
-
-    CryptUtils.decrypt_base64(encrypted_data, team_password)
-  end
-
-  def encrypt_data(team_password)
-    return if data.blank?
-
-    self.encrypted_data = CryptUtils.encrypt_base64(data, team_password)
+    encrypted_data.cleartext_value = { ose_secret: ose_secret }
+    encrypted_data.encrypt(team_password)
   end
 end
