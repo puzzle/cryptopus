b758b6a7e439480f72f4938143e0a08de1b642e5
Nils Rauch <rauch@puzzle.ch>
2020-12-11 11:37:27 +0100

    Add length to account description index

--- db/migrate/20201211072630_add_search_indexes.rb
+++ db/migrate/20201211072630_add_search_indexes.rb
@@ -1,6 +1,6 @@
 class AddSearchIndexes < ActiveRecord::Migration[6.0]
   def change
-    add_index :accounts, :description
+    add_index :accounts, :description, length: 150
     add_index :accounts, :accountname
     add_index :folders, :name
     add_index :teams, :name
