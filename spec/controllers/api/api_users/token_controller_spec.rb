61213abc75f16db655aadcf1e32f78685d65b2d7
Nils Rauch <rauch@puzzle.ch>
2020-12-17 13:30:24 +0100

    Expand specs

--- spec/controllers/api/api_users/token_controller_spec.rb
+++ spec/controllers/api/api_users/token_controller_spec.rb
@@ -5,30 +5,65 @@ require 'rails_helper'
 describe Api::ApiUsers::TokenController do
   include ControllerHelpers
 
-  before(:each) do
-    login_as(:bob)
-  end
-
   let(:api_user) { users(:bob).api_users.create!(description: 'my sweet api user') }
+  let(:api_user2) { users(:bob).api_users.create!(description: 'my sweet second api user') }
   let(:private_key) { users(:bob).decrypt_private_key('password') }
   let(:foreign_api_user) { users(:alice).api_users.create! }
 
   context 'GET show' do
-    it 'user renews token' do
-      @token = api_user.send(:decrypt_token, private_key)
-      @username = api_user.username
+    context 'as human user' do
+      before(:each) do
+        login_as(:bob)
+      end
 
-      get :show, params: { id: api_user.id }, xhr: true
+      it 'renews token of api user' do
+        old_token = api_user.send(:decrypt_token, private_key)
 
-      api_user.reload
+        get :show, params: { id: api_user.id }, xhr: true
 
-      expect(api_user).to_not be_locked
+        api_user.reload
+
+        new_token = api_user.send(:decrypt_token, private_key)
+        expect(api_user).to_not be_locked
+        expect(old_token).to_not eq(new_token)
+      end
+
+      it 'user cannot renew token of foreign_api_user' do
+        get :show, params: { id: foreign_api_user.id }, xhr: true
+
+        expect(response).to have_http_status(403)
+      end
     end
 
-    it 'user cannot renew token of foreign_api_user' do
-      get :show, params: { id: foreign_api_user.id }, xhr: true
+    context 'as api user' do
+      before(:each) do
+        request.headers['Authorization-User'] = api_user.username
+        request.headers['Authorization-Password'] = token
+        api_user.update!(valid_until: Time.zone.now + 5.minutes)
+      end
 
-      expect(response).to have_http_status(403)
+      it 'renews token of himself' do
+        old_token = api_user.send(:decrypt_token, private_key)
+
+        get :show, params: { id: api_user.id }, xhr: true
+
+        api_user.reload
+
+        new_token = api_user.send(:decrypt_token, private_key)
+        expect(old_token).to_not eq(new_token)
+      end
+
+      it 'cannot renew token of foreign_api_user' do
+        get :show, params: { id: foreign_api_user.id }, xhr: true
+
+        expect(response).to have_http_status(403)
+      end
+
+      it 'cannot renew token of different api user' do
+        get :show, params: { id: api_user2.id }, xhr: true
+
+        expect(response).to have_http_status(403)
+      end
     end
   end
 
@@ -46,7 +81,14 @@ describe Api::ApiUsers::TokenController do
     it 'user cannot invalidate token of foreign_api_user' do
       delete :destroy, params: { id: foreign_api_user.id }
 
-      expect(response).to have_http_status(403)
+      expect(response).to have_http_status(401)
     end
+  end
+
+  private
+
+  def token
+    decrypted_token = api_user.send(:decrypt_token, private_key)
+    Base64.encode64(decrypted_token)
   end
 end
--- spec/policies/user/api_policy_spec.rb
+++ spec/policies/user/api_policy_spec.rb
@@ -6,7 +6,10 @@ require 'user/api'
 describe User::ApiPolicy do
   include PolicyHelper
 
-  let!(:api_user) { users(:bob).api_users.create!(description: 'my sweet api user') }
+  let(:alice) { users(:alice) }
+  let(:bob) { users(:bob) }
+  let!(:api_user) { bob.api_users.create!(description: 'my sweet api user') }
+  let!(:foreign_api_user) { alice.api_users.create!(description: 'my sweet foreign api user') }
 
   context '#index' do
     it 'shows everyone their api users' do
@@ -32,6 +35,14 @@ describe User::ApiPolicy do
     it 'does not show a user a foreign api user' do
       refute_permit alice, api_user, :show?
     end
+
+    it 'shows an api user his api user' do
+      assert_permit api_user, api_user, :show?
+    end
+
+    it 'shows an api user a foreign api user' do
+      refute_permit foreign_api_user, api_user, :show?
+    end
   end
 
   context '#update' do
@@ -39,8 +50,16 @@ describe User::ApiPolicy do
       assert_permit bob, api_user, :update?
     end
 
-    it 'lets a user cannot update a foreign api user' do
+    it 'does not let a user update a foreign api user' do
       refute_permit alice, api_user, :update?
+    end
+
+    it 'lets an api user update his api user' do
+      assert_permit api_user, api_user, :show?
+    end
+
+    it 'does not let an api user update a foreign api user' do
+      refute_permit foreign_api_user, api_user, :show?
     end
   end
 
@@ -49,8 +68,16 @@ describe User::ApiPolicy do
       assert_permit bob, api_user, :destroy?
     end
 
-    it 'lets a user not delete a foreign api user' do
+    it 'does not let a user delete a foreign api user' do
       refute_permit alice, api_user, :destroy?
+    end
+
+    it 'lets an api user delete his api user' do
+      assert_permit api_user, api_user, :show?
+    end
+
+    it 'does not let an api user delete a foreign api user' do
+      refute_permit foreign_api_user, api_user, :show?
     end
   end
 
@@ -59,8 +86,16 @@ describe User::ApiPolicy do
       assert_permit bob, api_user, :lock?
     end
 
-    it 'lets a user not lock a foreign api user' do
+    it 'does not let a user lock a foreign api user' do
       refute_permit alice, api_user, :lock?
+    end
+
+    it 'lets an api user lock his api user' do
+      assert_permit api_user, api_user, :show?
+    end
+
+    it 'does not let an api user lock a foreign api user' do
+      refute_permit foreign_api_user, api_user, :show?
     end
   end
 
@@ -69,8 +104,16 @@ describe User::ApiPolicy do
       assert_permit bob, api_user, :unlock?
     end
 
-    it 'lets a user not unlock a foreign api user' do
+    it 'does not let a user unlock a foreign api user' do
       refute_permit alice, api_user, :unlock?
+    end
+
+    it 'lets an api user unlock his api user' do
+      assert_permit api_user, api_user, :show?
+    end
+
+    it 'does not let an api user unlock a foreign api user' do
+      refute_permit foreign_api_user, api_user, :show?
     end
   end
 end
