4a06ef9516a014beb309e9fe1a82cd59d410812f
e517403 <livio.liechti@sbb.ch>
2021-05-18 11:16:04 +0200

    Adjust ember nested component access, refs: #403

--- frontend/app/components/file-entry-row.js
+++
@@ -1,9 +0,0 @@
-import Component from "@glimmer/component";
-
-export default class FileEntryRowComponent extends Component {
-  get downloadLink() {
-    return `/api/accounts/${this.args.fileEntry.account.get(
-      "id"
-    )}/file_entries/${this.args.fileEntry.id}`;
-  }
-}
---
+++ frontend/tests/integration/components/account/card-show-test.js
@@ -0,0 +1,19 @@
+import { module, test } from "qunit";
+import { setupRenderingTest } from "ember-qunit";
+import { render } from "@ember/test-helpers";
+import { hbs } from "ember-cli-htmlbars";
+
+module("Integration | Component | account/card-show", function (hooks) {
+  setupRenderingTest(hooks);
+
+  test("it renders with data", async function (assert) {
+    this.set("account", {
+      accountname: "Ninjas account",
+      description: "Account for the ninjas"
+    });
+    await render(hbs`<Account::CardShow @account={{this.account}}/>`);
+    let text = this.element.textContent.trim();
+    assert.ok(text.includes("Ninjas account"));
+    assert.ok(text.includes("Account for the ninjas"));
+  });
+});
