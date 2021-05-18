3d7e37387403823c0187cbdbb80d077cb23bce92
e517403 <livio.liechti@sbb.ch>
2021-05-18 11:16:04 +0200

    Adjust ember nested component access, refs: #403

---
+++ frontend/app/components/api-user/table.js
@@ -0,0 +1,25 @@
+import Component from "@ember/component";
+import { action } from "@ember/object";
+import { inject as service } from "@ember/service";
+import { tracked } from "@glimmer/tracking";
+
+export default class Table extends Component {
+  @service store;
+
+  @tracked
+  renewMessage;
+
+  @action
+  createApiUser() {
+    this.store
+      .createRecord("user-api")
+      .save()
+      .then((apiUser) => {
+        this.apiUsers.addObject(apiUser);
+      });
+  }
+
+  setRenewMessage(message) {
+    this.renewMessage = message;
+  }
+}
