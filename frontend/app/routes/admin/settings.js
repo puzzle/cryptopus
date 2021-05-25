afc5abd2fe3bbebb5ba9b71fa8a5b9e5f40f907b
e517589 <lian_josko.vinkovic@sbb.ch>
2021-05-25 14:24:41 +0200

    fixed routes problem

--- frontend/app/routes/admin/settings.js
+++ frontend/app/routes/admin/settings.js
@@ -1,3 +1,15 @@
-import AdminRoute from "../admin";
+import Route from '@ember/routing/route';
+import { getNames } from 'ember-i18n-iso-countries';
 
-export default class SettingsRoute extends AdminRoute {}
+export default class Settings extends Route {
+    async model() {
+      const countriesArray = [];
+      const ISOArray = Object.entries(getNames("de"));
+      for (const country of ISOArray) {
+        countriesArray.push(country[1]);
+      }
+      return {
+        countryCodes: countriesArray,
+    }
+  }
+}
