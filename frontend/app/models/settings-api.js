994de92c744754b902b84067677a50f6357da469
Mio Sidler <sidler@puzzle.ch>
2021-06-01 10:19:01 +0200

    Add component for power-select-with-create, add translations ch-de

--- frontend/app/models/settings-api.js
+++
@@ -1,6 +0,0 @@
-import Model, { attr } from "@ember-data/model";
-
-export default class SettingsApi extends Model {
-    @attr("string") key;
-    @attr("string[]") value;
-}
--- frontend/app/routes/admin/settings.js
+++ frontend/app/routes/admin/settings.js
@@ -5,11 +5,14 @@ import RSVP from "rsvp";
 export default class AdminSettingsRoute extends AdminRoute {
   model() {
     const ISOArray = Object.entries(getNames("de"));
+    const ipArray = [];
     const countries = ISOArray.map((countryArray) => {
       return { value: countryArray[0], label: countryArray[1] };
     });
+    const ipAdresses = [];
     return RSVP.hash({
       countries: countries,
+      ipAdresses: ipAdresses,
     });
   }
 }
