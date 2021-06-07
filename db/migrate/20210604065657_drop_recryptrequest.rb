fa46abec4f770a29c8b731b3a9186df38e2cca9c
Luca Brügger <hello@lucabruegger.ch>
2021-06-07 08:33:10 +0200

    Change migration

--- db/migrate/20210604065657_drop_recryptrequest.rb
+++ db/migrate/20210604065657_drop_recryptrequest.rb
@@ -1,6 +1,6 @@
 class DropRecryptrequest < ActiveRecord::Migration[6.1]
   def change
     drop_table :recryptrequests
-    remove_column :users, :recryptrequests
+    remove_column :user, :recryptrequests
   end
 end
