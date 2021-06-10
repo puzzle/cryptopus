fd0135528f7b3a4cae95c50ef06bdf7feef3100b
Mio Sidler <sidler@puzzle.ch>
2021-06-10 15:31:02 +0200

    Run frontend:prepare only once in ci.rake

--- bin/prepare-frontend.sh
+++ bin/prepare-frontend.sh
@@ -2,5 +2,5 @@
 yarn --cwd frontend/ install
 yarn --cwd frontend/ build-prod
 rm public/frontend-index.html
-rsync --ignore-existing -r frontend/dist/* public/
+rsync -r frontend/dist/* public/
 mv public/{index,frontend-index}.html
