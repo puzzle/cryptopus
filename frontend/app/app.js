82cb942335e5c8100ed5c3236478ca43099bc7fe
Luca Brügger <hello@lucabruegger.ch>
2021-06-21 15:36:01 +0200

    Begin with PuzzleShell fixes

--- frontend/app/app.js
+++ frontend/app/app.js
@@ -2,6 +2,7 @@ import Application from "@ember/application";
 import Resolver from "./resolver";
 import loadInitializers from "ember-load-initializers";
 import config from "./config/environment";
+// import "@puzzleitc/puzzle-shell/index";
 
 export default class App extends Application {
   modulePrefix = config.modulePrefix;
