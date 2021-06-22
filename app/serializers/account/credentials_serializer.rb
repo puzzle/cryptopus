f29d615cef95033b495023fb1229e27dfc0a6d32
e517589 <lian_josko.vinkovic@sbb.ch>
2021-06-22 16:52:44 +0200

    Add creation and updated date to account show, refs: #310

--- app/serializers/account/credentials_serializer.rb
+++ app/serializers/account/credentials_serializer.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
 
 class Account::CredentialsSerializer < AccountSerializer
-  attributes :cleartext_username, :cleartext_password
+  attributes :cleartext_username, :cleartext_password, :created_at, :updated_at
 end
