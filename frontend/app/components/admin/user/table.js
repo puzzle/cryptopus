30e4b890f2ddf392407d5f8b914a3a46a138d35d
Nils Rauch <nils.rauch@gmail.com>
2021-07-01 11:26:28 +0200

    Add unlock button to user/table-row, refs: #453

--- frontend/app/components/admin/user/table.js
+++ frontend/app/components/admin/user/table.js
@@ -1,16 +1,22 @@
 import Component from "@glimmer/component";
 import { tracked } from "@glimmer/tracking";
+import { action } from "@ember/object";
 
 export default class AdminUsersTable extends Component {
   @tracked
   isUserNew = false;
 
+  @tracked
+  users = [];
+
   constructor() {
     super(...arguments);
+
+    if (this.args.users) this.users = this.args.users.toArray();
   }
 
   get sortedUsers() {
-    return this.args.users
+    return this.users
       .filter((user) => {
         return !user.isDeleted;
       })
@@ -19,5 +25,10 @@ export default class AdminUsersTable extends Component {
         if (userA.username > userB.username) return 1;
         return 0;
       });
+  }
+
+  @action
+  removeUser(user) {
+    this.users.removeObject(user);
   }
 }
