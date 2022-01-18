602f097d05c4cc53d00b2a582e00ec810b7e55e3
Mountain Star <github@mtnstar.net>
2022-01-18 14:51:10 +0100

    Fix policy specs

--- spec/policies/encryptable_policy_spec.rb
+++ spec/policies/encryptable_policy_spec.rb
@@ -4,47 +4,46 @@ require 'spec_helper'
 describe EncryptablePolicy do
   include PolicyHelper
 
-  let(:credential1) { encryptables(:credential1) }
-  let(:credential2) { encryptables(:credential2) }
+  let(:credentials2) { encryptables(:credentials2) }
   let(:folder2) { folders(:folder2) }
   let(:team1) { teams(:team1) }
   let(:team2) { teams(:team2) }
 
   context 'as teammember' do
     it 'can show credential' do
-      assert_permit bob, credential2, :show?
+      assert_permit bob, credentials2, :show?
     end
 
     it 'can destroy credential' do
-      assert_permit bob, credential2, :destroy?
+      assert_permit bob, credentials2, :destroy?
     end
   end
 
   context 'as non teammember' do
     it 'non teammember cannot show credential' do
-      refute_permit alice, credential2, :show?
+      refute_permit alice, credentials2, :show?
     end
 
     it 'non teammember cannot destroy credential' do
-      refute_permit alice, credential2, :destroy?
+      refute_permit alice, credentials2, :destroy?
     end
 
   end
 
   context 'for team' do
     it 'enabled api user can show credential' do
-      assert_permit api_bob, credential2, :show?
+      assert_permit api_bob, credentials2, :show?
     end
 
     it 'enabled api user cannot destroy credential' do
-      refute_permit api_bob, credential2, :destroy?
+      refute_permit api_bob, credentials2, :destroy?
     end
 
   end
 
   context 'not for team' do
     it 'enabled api user is not allowed to update credential' do
-      refute_permit api_alice, credential2, :update?
+      refute_permit api_alice, credentials2, :update?
     end
   end
 
