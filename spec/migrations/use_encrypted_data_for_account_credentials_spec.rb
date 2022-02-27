824d554fae97332e61cb06ba82241eb421abc060
Mountain Star <github@mtnstar.net>
2022-02-27 19:45:46 +0100

    Fix migration specs

--- spec/migrations/use_encrypted_data_for_account_credentials_spec.rb
+++ spec/migrations/use_encrypted_data_for_account_credentials_spec.rb
@@ -203,7 +203,8 @@ class LegacyAccountCredentialsAfter < ApplicationRecord
     encrypted_value = if cleartext_value.blank?
                         nil
                       else
-                        Crypto::Symmetric::Aes256.encrypt(cleartext_value, team_password)
+                        require 'pry'; binding.pry unless $pstop
+                          Crypto::Symmetric::Aes256.encrypt(cleartext_value, team_password)
                       end
     encrypted_data.[]=(attr, **{ data: encrypted_value, iv: nil })
   end
@@ -232,30 +233,13 @@ class LegacyAccountCredentialsAfter < ApplicationRecord
       decrypt_attr(:password, team_password)
     end
 
-    def encrypt(team_password)
-      encrypt_attr(:username, team_password)
-      encrypt_attr(:password, team_password)
-    end
-
     private
-
-    def encrypt_attr(attr, team_password)
-      cleartext_value = send(:"cleartext_#{attr}")
-
-      encrypted_value = if cleartext_value.blank?
-                          nil
-                        else
-                          CryptUtils.encrypt_blob(cleartext_value, team_password)
-                        end
-
-      encrypted_data[attr] = { data: encrypted_value, iv: nil }
-    end
 
     def decrypt_attr(attr, team_password)
       encrypted_value = encrypted_data[attr].try(:[], :data)
 
       cleartext_value = if encrypted_value
-                          CryptUtils.decrypt_blob(encrypted_value, team_password)
+                          Crypto::Symmetric::AES256.decrypt(encrypted_value, team_password)
                         end
 
       instance_variable_set("@cleartext_#{attr}", cleartext_value)
