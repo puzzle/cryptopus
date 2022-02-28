a44f90a0b26ca8f4964c9887fa3cc57d6cfc5c15
Mountain Star <github@mtnstar.net>
2022-02-28 14:02:08 +0100

    Make rubocop happy

--- spec/system/encryptable_files_modal_system_spec.rb
+++ spec/system/encryptable_files_modal_system_spec.rb
@@ -4,83 +4,83 @@ require 'spec_helper'
 
 describe 'Encryptable Files Modal', type: :system, js: true do
   # TODO: adjust tests after implementing/fixing encryptable files modal
-#   include SystemHelpers
+  #   include SystemHelpers
 
   # let(:bob) { users(:bob) }
   # let(:bobs_private_key) { bob.decrypt_private_key('password') }
   # let(:plaintext_team_password) { teams(:team1).decrypt_team_password(bob, bobs_private_key) }
 
   # it 'creates and deletes a file entry' do
-    # login_as_user(:bob)
+  # login_as_user(:bob)
 
-    # team = teams(:team1)
-    # team_password = team.decrypt_team_password(bob, bobs_private_key)
+  # team = teams(:team1)
+  # team_password = team.decrypt_team_password(bob, bobs_private_key)
 
-    # credentials = encryptables(:credentials1)
-    # credentials.decrypt(team_password)
-    # visit("/encryptables/#{credentials.id}")
+  # credentials = encryptables(:credentials1)
+  # credentials.decrypt(team_password)
+  # visit("/encryptables/#{credentials.id}")
 
-    # expect_encryptable_page_with(credentials)
+  # expect_encryptable_page_with(credentials)
 
-    # # Create File Entry
-    # file_name = 'test_file.txt'
-    # file_desc = 'description'
-    # file_path = "#{Rails.root}/spec/fixtures/files/#{file_name}"
-    # expect do
-      # create_new_file_entry(file_desc, file_path)
-      # expect(page).to have_text(credentials.name)
-    # end.to change { FileEntry.count }.by(1)
+  # # Create File Entry
+  # file_name = 'test_file.txt'
+  # file_desc = 'description'
+  # file_path = "#{Rails.root}/spec/fixtures/files/#{file_name}"
+  # expect do
+  # create_new_file_entry(file_desc, file_path)
+  # expect(page).to have_text(credentials.name)
+  # end.to change { FileEntry.count }.by(1)
 
-    # file_entry = FileEntry.find_by(filename: 'test_file.txt')
-    # file_content = fixture_file_upload(file_path, 'text/plain').read
-    # file_entry.decrypt(plaintext_team_password)
-    # expect(file_entry.cleartext_file).to eq file_content
+  # file_entry = FileEntry.find_by(filename: 'test_file.txt')
+  # file_content = fixture_file_upload(file_path, 'text/plain').read
+  # file_entry.decrypt(plaintext_team_password)
+  # expect(file_entry.cleartext_file).to eq file_content
 
-    # expect_encryptable_page_with(credentials)
+  # expect_encryptable_page_with(credentials)
 
-    # # Try to upload again
-    # expect do
-      # create_new_file_entry(file_desc, file_path)
-    # end.to change { FileEntry.count }.by(0)
+  # # Try to upload again
+  # expect do
+  # create_new_file_entry(file_desc, file_path)
+  # end.to change { FileEntry.count }.by(0)
 
-    # expect(page).to have_text('Filename is already taken')
-    # click_button('Close')
+  # expect(page).to have_text('Filename is already taken')
+  # click_button('Close')
 
-    # expect(page).to have_text("Encryptable: #{credentials.name}")
+  # expect(page).to have_text("Encryptable: #{credentials.name}")
 
-    # # Delete File Entry
-    # del_button = all('img[alt="delete"]')[3]
-    # expect(del_button).to be_present
+  # # Delete File Entry
+  # del_button = all('img[alt="delete"]')[3]
+  # expect(del_button).to be_present
 
-    # del_button.click
-    # find('button', text: 'Delete').click
-    # expect(all('tr').count).to eq(3)
+  # del_button.click
+  # find('button', text: 'Delete').click
+  # expect(all('tr').count).to eq(3)
   # end
 
   # private
 
   # def create_new_file_entry(description, file_path)
-    # new_file_entry_button = find('button.btn.btn-primary', text: 'Add Attachment', visible: false)
-    # new_file_entry_button.click
+  # new_file_entry_button = find('button.btn.btn-primary', text: 'Add Attachment', visible: false)
+  # new_file_entry_button.click
 
-    # expect(page).to have_text('Add new attachment to encryptable')
+  # expect(page).to have_text('Add new attachment to encryptable')
 
-    # expect(find('.modal-content')).to be_present
-    # expect(page).to have_button('Upload')
+  # expect(find('.modal-content')).to be_present
+  # expect(page).to have_button('Upload')
 
-    # within('div.modal-content') do
-      # fill_in 'description', with: description
-      # find(:xpath, "//input[@type='file']", visible: false).attach_file(file_path)
-    # end
+  # within('div.modal-content') do
+  # fill_in 'description', with: description
+  # find(:xpath, "//input[@type='file']", visible: false).attach_file(file_path)
+  # end
 
-    # click_button 'Upload'
+  # click_button 'Upload'
   # end
 
   # def expect_encryptable_page_with(credentials)
-    # expect(first('h2')).to have_text("Encryptable: #{credentials.name}")
-    # expect(find('#cleartext_username').value).to eq(credentials.cleartext_username)
-    # expect(find('#cleartext_password', visible: false).value).to eq(credentials.cleartext_password)
-    # expect(page).to have_text(credentials.description)
+  # expect(first('h2')).to have_text("Encryptable: #{credentials.name}")
+  # expect(find('#cleartext_username').value).to eq(credentials.cleartext_username)
+  # expect(find('#cleartext_password', visible: false).value).to eq(credentials.cleartext_password)
+  # expect(page).to have_text(credentials.description)
   # end
 
 end
