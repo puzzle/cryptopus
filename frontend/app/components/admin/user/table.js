4021ebb77a4b32a7c3ecdcd864d050a9804c80b9
RamonaChristen <ramona.christen@students.unibe.ch>
2022-03-15 15:56:20 +0100

    add descending sort

--- frontend/app/components/admin/user/table.js
+++ frontend/app/components/admin/user/table.js
@@ -9,12 +9,25 @@ export default class AdminUsersTable extends Component {
   @tracked
   users = [];
 
+  sortedBy = "";
+
   constructor() {
     super(...arguments);
 
-    if (this.args.users) this.users = this.args.users.toArray();
+    if (this.args.users) {
+      this.users = this.args.users
+      .toArray().filter((user) => {
+          return !user.isDeleted;
+        })
+        .sort((userA, userB) => {
+          if (userA.username < userB.username) return -1;
+          if (userA.username > userB.username) return 1;
+        return 0;
+      });
+    }
   }
 
+  /*
   get sortedUsers() {
     return this.users
       .filter((user) => {
@@ -26,6 +39,7 @@ export default class AdminUsersTable extends Component {
         return 0;
       });
   }
+  */
 
   @action
   removeUser(user) {
@@ -34,49 +48,62 @@ export default class AdminUsersTable extends Component {
 
   @action
   sortBy(attribute) {
-    this.users = this.users.sort((userA, userB) => {
-      switch (attribute.toString()) {
-        case "username":
-          if (userA.username < userB.username) return -1;
-          if (userA.username > userB.username) return 1;
-          return 0;
-        case "givenname":
-          if (userA.givenname < userB.givenname) return -1;
-          if (userA.givenname > userB.givenname) return 1;
-          return 0;
-        case "last_login_at": // works but could be prettier
-          if (userA.lastLoginAt == null && userB.lastLoginAt == null) {
+    let asc;
+    if (attribute == this.sortedBy) {
+      asc = -1;
+      this.sortedBy = "";
+    } else {
+      asc = 1;
+      this.sortedBy = attribute.toString();
+    }
+
+    this.users = this.users
+      .filter((user) => {
+        return !user.isDeleted;
+      })
+      .sort((userA, userB) => {
+        switch (attribute.toString()) {
+          case "username":
+            if (userA.username < userB.username) return -1 * asc;
+            if (userA.username > userB.username) return 1 * asc;
             return 0;
-          } else if (userA.lastLoginAt == null) {
-            return 1;
-          } else if (userB.lastLoginAt == null) {
-            return -1;
-          } else {
-            if (userA.lastLoginAt < userB.lastLoginAt) return -1;
-            if (userA.lastLoginAt > userB.lastLoginAt) return 1;
+          case "givenname":
+            if (userA.givenname < userB.givenname) return -1 * asc;
+            if (userA.givenname > userB.givenname) return 1 * asc;
             return 0;
-          }
-        case "last_login_from":
-          if (userA.lastLoginFrom == null && userB.lastLoginFrom == null) {
+          case "last_login_at": // works but could be prettier
+            if (userA.lastLoginAt == null && userB.lastLoginAt == null) {
+              return 0;
+            } else if (userA.lastLoginAt == null) {
+              return 1;
+            } else if (userB.lastLoginAt == null) {
+              return -1;
+            } else {
+              if (userA.lastLoginAt < userB.lastLoginAt) return -1 * asc;
+              if (userA.lastLoginAt > userB.lastLoginAt) return 1 * asc;
+              return 0;
+            }
+          case "last_login_from":
+            if (userA.lastLoginFrom == null && userB.lastLoginFrom == null) {
+              return 0;
+            } else if (userA.lastLoginFrom == null) {
+              return 1;
+            } else if (userB.lastLoginFrom == null) {
+              return -1;
+            } else {
+              if (userA.lastLoginFrom < userB.lastLoginFrom) return -1 * asc;
+              if (userA.lastLoginFrom > userB.lastLoginFrom) return 1 * asc;
+              return 0;
+            }
+          case "auth":
+            if (userA.auth < userB.auth) return -1 * asc;
+            if (userA.auth > userB.auth) return 1 * asc;
             return 0;
-          } else if (userA.lastLoginFrom == null) {
-            return 1;
-          } else if (userB.lastLoginFrom == null) {
-            return -1;
-          } else {
-            if (userA.lastLoginFrom < userB.lastLoginFrom) return -1;
-            if (userA.lastLoginFrom > userB.lastLoginFrom) return 1;
+          case "role":
+            if (userA.role < userB.role) return -1 * asc;
+            if (userA.role > userB.role) return 1 * asc;
             return 0;
-          }
-        case "auth":
-          if (userA.auth < userB.auth) return -1;
-          if (userA.auth > userB.auth) return 1;
-          return 0;
-        case "role":
-          if (userA.role < userB.role) return -1;
-          if (userA.role > userB.role) return 1;
-          return 0;
-      }
-    });
+        }
+      });
   }
 }
