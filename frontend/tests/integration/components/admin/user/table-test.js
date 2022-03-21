eced3ceac1575d8b81a29e0e154d207340c56ba7
RamonaChristen <72438320+RamonaChristen@users.noreply.github.com>
2022-03-21 12:18:53 +0100

    Feature/541 provide sorting in admin/users view (#575)
    * add sort function for all attributes to table.js
    
    * add first working version for sorting table
    
    * add descending sort
    
    * Fixed format of the code to pass test.
    
    * Fixed test. All tests pass now.
    
    * improve sort functions for last_login_at and auth/UID, add tests
    
    * Extended sort-test and corrected typo
    
    * delete unused code, fix bug
    
    * Update frontend/app/components/admin/user/table.js
    
    Co-authored-by: Luca Brügger <52595924+luca-bruegger@users.noreply.github.com>
    
    * add third condition to ternary, fix bug
    
    * implement suggested changes
    
    * implement suggested changes in test file
    
    * make name sorting case insensitive, sort by surname if same givenname
    
    Co-authored-by: Julien Gaumez <julien.gaumez@students.unibe.ch>
    Co-authored-by: Luca Brügger <52595924+luca-bruegger@users.noreply.github.com>

--- frontend/tests/integration/components/admin/user/table-test.js
+++ frontend/tests/integration/components/admin/user/table-test.js
@@ -1,6 +1,6 @@
 import { module, test } from "qunit";
 import { setupRenderingTest } from "ember-qunit";
-import { render } from "@ember/test-helpers";
+import { render, click } from "@ember/test-helpers";
 import { hbs } from "ember-cli-htmlbars";
 import { setLocale } from "ember-intl/test-support";
 import ENV from "../../../../../config/environment";
@@ -17,22 +17,34 @@ module("Integration | Component | admin/user/table", function (hooks) {
     ENV.currentUserGivenname = null;
   });
 
-  test("it renders with data", async function (assert) {
-    const users = [
-      {
-        username: "Bob",
-        isDeleted: false
-      },
-      {
-        username: "Alice",
-        isDeleted: false
-      },
-      {
-        username: "Fred",
-        isDeleted: true
-      }
-    ];
+  const users = [
+    {
+      username: "Bob",
+      givenname: "Bobby",
+      role: "admin",
+      lastLoginAt: "11.03.2022 03:03",
+      lastLoginFrom: "111.123.22.2",
+      auth: "db",
+      providerUid: 2,
+      isDeleted: false
+    },
+    {
+      username: "Alice",
+      givenname: "Allison",
+      role: "user",
+      lastLoginAt: "12.03.2022 06:56",
+      lastLoginFrom: "111.123.22.1",
+      auth: "db",
+      providerUid: 1,
+      isDeleted: false
+    },
+    {
+      username: "Fred",
+      isDeleted: true
+    }
+  ];
 
+  test("it renders with data", async function (assert) {
     this.set("users", users);
 
     await render(hbs`<Admin::User::Table @users={{this.users}} />`);
@@ -51,5 +63,43 @@ module("Integration | Component | admin/user/table", function (hooks) {
     assert.ok(text.includes("Alice"));
 
     assert.ok(!text.includes("Fred"));
+  });
+
+  test("sorting works", async function (assert) {
+    this.set("users", users);
+
+    await render(hbs`<Admin::User::Table @users={{this.users}} />`);
+    let text = this.element.textContent.trim();
+
+    assert.ok(text.indexOf("Alice") < text.indexOf("Bob"));
+
+    assert.ok(proveDescOrder("sort-username", "Alice", "Bob", this));
+
+    assert.ok(proveAscOrder("sort-name", "Alice", "Bob", this));
+    assert.ok(proveDescOrder("sort-name", "Alice", "Bob", this));
+
+    assert.ok(proveAscOrder("sort-role", "admin", "user", this));
+    assert.ok(proveDescOrder("sort-role", "admin", "user", this));
+
+    assert.ok(proveAscOrder("sort-auth", "Alice", "Bob", this));
+    assert.ok(proveDescOrder("sort-auth", "Alice", "Bob", this));
+
+    assert.ok(proveAscOrder("sort-login-at", "Alice", "Bob", this));
+    assert.ok(proveDescOrder("sort-login-at", "Alice", "Bob", this));
+
+    assert.ok(proveAscOrder("sort-login-from", "Alice", "Bob", this));
+    assert.ok(proveDescOrder("sort-login-from", "Alice", "Bob", this));
+
+    async function proveAscOrder(elementId, value1, value2, testObject) {
+      await click(`span[id='${elementId}']`);
+      text = testObject.element.textContent.trim();
+      return text.indexOf(value1) < text.indexOf(value2);
+    }
+
+    async function proveDescOrder(elementId, value1, value2, testObject) {
+      await click(`span[id='${elementId}']`);
+      text = testObject.element.textContent.trim();
+      return text.indexOf(value1) > text.indexOf(value2);
+    }
   });
 });
