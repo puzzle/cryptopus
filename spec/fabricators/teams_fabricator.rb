ea6753f5aea9ada68f0a27c035f18f3e869d4626
Dario Marti <dario.marti@fadendaten.ch>
2022-03-21 20:39:19 +0100

    Remove Aes snake case and rebase with master

--- spec/fabricators/teams_fabricator.rb
+++ spec/fabricators/teams_fabricator.rb
@@ -8,7 +8,7 @@ Fabricator(:non_private_team, from: :team) do |t|
   t.visible true
   t.private false
   after_save do |team|
-    team_password = Crypto::Symmetric::Aes256.random_key
+    team_password = Crypto::Symmetric::AES256.random_key
     team.add_user(Fabricate(:user), team_password)
     User::Human.admins.each do |a|
       team.add_user(a, team_password)
@@ -24,7 +24,7 @@ Fabricator(:private_team, from: :team) do |t|
   t.visible true
   t.private true
   after_save do |team|
-    team_password = Crypto::Symmetric::Aes256.random_key
+    team_password = Crypto::Symmetric::AES256.random_key
     team.add_user(Fabricate(:user), team_password)
     folder = Fabricate(:folder, team: team)
     Fabricate(:credential, folder: folder, team_password: team_password)
