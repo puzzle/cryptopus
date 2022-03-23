d7e87c62908e407f9aa253d8ad06b28db554bc61
Niklas Jäggi <jaeggi@puzzle.ch>
2022-03-23 17:01:58 +0100

    personal_team gets created when user logs into account without a personal_team

--- db/schema.rb
+++ db/schema.rb
@@ -89,6 +89,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_03_29_122335) do
     t.text "options"
     t.integer "role", default: 0, null: false
     t.integer "default_ccli_user_id"
+    t.integer "personal_team_id"
     t.index ["default_ccli_user_id"], name: "index_users_on_default_ccli_user_id"
   end
 
