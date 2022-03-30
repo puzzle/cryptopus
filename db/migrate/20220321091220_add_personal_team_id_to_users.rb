10438688061d738e8f9670d3d80eb469f2be6e38
Niklas Jäggi <jaeggi@puzzle.ch>
2022-03-30 15:10:15 +0200

    create personal_team for every already existing user

--- db/migrate/20220321091220_add_personal_team_id_to_users.rb
+++ db/migrate/20220321091220_add_personal_team_id_to_users.rb
@@ -1,5 +1,9 @@
 class AddPersonalTeamIdToUsers < ActiveRecord::Migration[6.1]
   def change
     add_column :users, :personal_team_id, :integer
+
+    User.all.find_each do |user|
+      user.create_personal_team!
+    end
   end
 end
