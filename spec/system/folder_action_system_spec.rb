4ad0606420e5c70643d419b3104c211e27eaa01c
renato-oester <renato.oester@bluewin.ch>
2022-04-01 16:01:16 +0200

    adds additional tests for folder actions

--- spec/system/folder_action_system_spec.rb
+++ spec/system/folder_action_system_spec.rb
@@ -4,7 +4,7 @@ require 'spec_helper'
 
 describe 'FolderAction', type: :system, js: true do
   include SystemHelpers
-# can select folder and team in side-navbar 
+# can select folder and team from side-navbar 
   it 'opens folder and team in side-navbar' do
     login_as_user(:admin)
     visit('/')
@@ -24,27 +24,29 @@ describe 'FolderAction', type: :system, js: true do
     first('.list-group-item.list-folder-item.bg-blue-one').click
     
     expect_folder_expanded
-
-
-    logout
-  end
-#prüft ob Inhalt vorhanden 
-  it 'shows folder content drop down' do
-    login_as_user(:tux)
-
-    within(page.find('pzsh-menu')) do
-      expect(page).to have_text('Admin')
+    
+    
+    within(page.first('.row.d-flex.align-items-center.p-2.bg-grey-2.rounded.encryptable-entry')) do
+      expect(page).to have_text('Personal Mailbox')
     end
-
     logout
-    
   end
-    
-  it 'shows folder content not dropdown' do
-    login_as_user(:tux)
 
+# can select folder from team card 
+  it 'opens folder in team card' do
+    login_as_user(:admin)
+    visit('/teams/235930340')
+    
+    within(page.first('pl-2.pr-2.folder-card-header')) do
+      expect(page).to have_text('folder1')  
+    end
+    
+    first('pl-2.pr-2.folder-card-header').click
+    
+    within(page.first('.row.d-flex.align-items-center.p-2.bg-grey-2.rounded.encryptable-entry')) do
+      expect(page).to have_text('Personal Mailbox')
+    end
     logout
-  
   end
 #
   it 'shows preselected' do
