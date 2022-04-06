208e9af550334991a4e4a39b005b440df2483d16
Julien Gaumez <julien.gaumez@students.unibe.ch>
2022-04-06 17:07:29 +0200

    Started frontend implementation of Log butten/page not working atm.

---
+++ frontend/app/routes/log.js
@@ -0,0 +1,23 @@
+import BaseRoute from "./base";
+import ENV from "../config/environment";
+import RSVP from "rsvp";
+import { inject as service } from "@ember/service";
+
+export default class LogRoute extends BaseRoute {
+  @service navService;
+
+  model() {
+    return RSVP.hash({
+      info: {
+        lastLoginAt: ENV.currentUserLastLoginAt,
+        lastLoginFrom: ENV.currentUserLastLoginFrom,
+        currentUserLabel: ENV.currentUserLabel
+      },
+      apiUsers: this.store.findAll("user-api")
+    });
+  }
+
+  afterModel() {
+    this.navService.clear();
+  }
+}
