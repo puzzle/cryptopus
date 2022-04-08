4d42a8058d8aa247f3c9db8cb1ef279d235b0326
Julien Gaumez <julien.gaumez@students.unibe.ch>
2022-04-08 16:35:34 +0200

    Revert "deleted unused code" Not smart to do this in this branch
    This reverts commit 7c435b20d0139f13c5bbc815a7474d6a6bb0ae4c.

--- frontend/app/components/nav-bar.js
+++ frontend/app/components/nav-bar.js
@@ -1,5 +1,7 @@
 import Component from "@glimmer/component";
 import { inject as service } from "@ember/service";
+import { action } from "@ember/object";
+import { tracked } from "@glimmer/tracking";
 import ENV from "../config/environment";
 
 export default class NavBarComponent extends Component {
@@ -10,11 +12,59 @@ export default class NavBarComponent extends Component {
 
   searchInterval;
 
+  @tracked
+  isNewEncryptable = false;
+
+  @tracked
+  isNewFolder = false;
+
+  @tracked
+  isNewTeam = false;
+
   get isStartpage() {
     return this.router.currentRouteName === "index";
   }
 
   get givenname() {
     return ENV.currentUserGivenname;
+  }
+
+  @action
+  setNavbarCollapsed(isCollapsed) {
+    this.navService.isNavbarCollapsed = isCollapsed;
+  }
+
+  @action
+  toggleEncryptableCreating() {
+    this.isNewEncryptable = !this.isNewEncryptable;
+  }
+
+  @action
+  toggleFolderCreating() {
+    this.isNewFolder = !this.isNewFolder;
+  }
+
+  @action
+  toggleTeamCreating() {
+    this.isNewTeam = !this.isNewTeam;
+  }
+
+  @action
+  searchByQuery() {
+    clearInterval(this.searchInterval);
+    this.searchInterval = setInterval(() => {
+      if (
+        this.navService.searchQuery &&
+        this.navService.searchQuery.trim(" ").length > 2
+      ) {
+        this.router.transitionTo("teams.index", {
+          queryParams: {
+            q: this.navService.searchQuery,
+            team_id: undefined,
+            folder_id: undefined
+          }
+        });
+      }
+    }, 800);
   }
 }
