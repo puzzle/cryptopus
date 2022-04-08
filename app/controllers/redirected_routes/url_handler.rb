24dd0767656425d696f8b929d15ccb89f0e76dab
Julien Gaumez <julien.gaumez@students.unibe.ch>
2022-04-08 13:23:16 +0200

    Fixed refresh bug when on /log page. (the bug mentioned in earlier commit)

--- app/controllers/redirected_routes/url_handler.rb
+++ app/controllers/redirected_routes/url_handler.rb
@@ -10,6 +10,7 @@ class RedirectedRoutes::UrlHandler
     folder_show: /(^\/teams\/\d+\/folders\/\d+)$/,
     encryptables_show: /(^\/encryptables\/\d+)$/,
     profile_index: /(^\/profile)$/,
+    log_index: /(^\/log)$/,
     admin_users_index: /(^\/admin\/users)$/,
     admin_settings: /(^\/admin\/settings)$/,
     dashboard: /(^\/dashboard)$/
