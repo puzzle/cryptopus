81548f86f13e7e652b25f63ea80afb03e4482e47
MuStAgEDelta <ra.fe@live.de>
2022-04-11 14:42:52 +0200

    change a 'not' mistake

--- spec/system/personal_log_system_spec.rb
+++ spec/system/personal_log_system_spec.rb
@@ -9,6 +9,7 @@ it 'as admin is there everything avaible' do
     login_as_root
     visit('/log')
     expect(page).to have_text('Personal activity log') 
+    #no side bar
     expect(page).not_to have_text('button id="ember4608"')
     expect(page).to have_text('Action')
     expect(page).to have_text('Date')
@@ -21,7 +22,7 @@ it 'is the Log correct' do
 #action 
     expect(page).to have_text('these are not implemented yet') 
     #Date
-    expect(page).not_to have_text('this neither')
+    expect(page).to have_text('this neither')
     #Change
     expect(page).to have_text('nope just example')
     logout
@@ -33,7 +34,8 @@ it 'as user is there everything avaible' do
 
     expect(page).to have_text('Personal activity log') 
     expect(page).to have_text('Personal activity log') 
-    expect(page).to have_text('button id="ember4608"')
+    #no side bar
+    expect(page).not_to have_text('button id="ember4608"')
     expect(page).to have_text('Action')
     expect(page).to have_text('Date')
     expect(page).to have_text('Change')    
