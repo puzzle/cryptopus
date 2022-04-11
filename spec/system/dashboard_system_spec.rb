053d3b891a3ae169c63d4c6749ab92741997f02a
Renato Oester <renato.oester@bluewin.ch>
2022-04-11 15:39:23 +0200

    Adds system specs for encryptables on dashboard

--- spec/system/dashboard_system_spec.rb
+++ spec/system/dashboard_system_spec.rb
@@ -12,12 +12,13 @@ describe 'Dashboard', type: :system, js: true do
 
     expect(page).to have_selector('pzsh-hero', visible: true)
     expect(page).to have_selector('pzsh-banner', visible: true)
+    expect(page).to have_text('Recent Credentials', count: 1)
     expect(page).to have_text('Favourites', count: 1)
     expect(page).to have_text('Teams', count: 1)
 
     expect(page).not_to have_selector 'div.content'
 
-    expect(page).to have_selector('div.dashboard-grid-card', count: 2)
+    expect(page).to have_selector('div.dashboard-grid-card', count: 3)
   end
 
   it 'navigates to team on team card click' do
@@ -26,9 +27,27 @@ describe 'Dashboard', type: :system, js: true do
     expect(page.current_path).to eq('/dashboard')
 
     expect(page).to have_selector('pzsh-hero', visible: true)
-    all('div.dashboard-grid-card').first.click
 
-    expect(page.current_path).to eq('/teams/235930340')
+    team = Team.first
+
+    first('div.dashboard-grid-card', text: team.name).click
+
+    expect(page.current_path).to eq("/teams/#{team.id}")
+    expect(page).to have_selector 'div.content'
+  end
+  
+  it 'navigates to encryptable on encryptable card click' do
+    login_as_user(:alice)
+
+    expect(page.current_path).to eq('/dashboard')
+
+    expect(page).to have_selector('pzsh-hero', visible: true)
+
+    encryptable = Encryptable.first
+
+    first('div.dashboard-grid-card', text: encryptable.name).click
+
+    expect(page.current_path).to eq("/encryptables/#{encryptable.id}")
     expect(page).to have_selector 'div.content'
   end
 end
