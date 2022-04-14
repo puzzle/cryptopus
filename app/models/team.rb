fea7ad3072202e22085255bf9e4877e78ef0a7b2
Niklas Jäggi <jaeggi@puzzle.ch>
2022-04-14 16:33:27 +0200

    added dependent option to personal_team_owner

--- app/models/team.rb
+++ app/models/team.rb
@@ -20,8 +20,8 @@ class Team < ApplicationRecord
   has_many :folders, -> { order :name }, dependent: :destroy
   has_many :teammembers, dependent: :delete_all
   has_many :members, through: :teammembers, source: :user
-  has_one :personal_team_owner, class_name: 'User::Human', inverse_of: 'personal_team',
-          foreign_key: :personal_team_id
+  has_one :personal_team_owner, class_name: 'User::Human', inverse_of: 'personal_team', foreign_key:
+    :personal_team_id, dependent: nil
   has_many :user_favourite_teams, dependent: :destroy
 
   validates :name, presence: true
