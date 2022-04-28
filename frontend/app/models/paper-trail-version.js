b991304ee30a04b9acc45d264dcb477cb39971a4
RamonaChristen <ramona.christen@students.unibe.ch>
2022-04-28 13:27:29 +0200

    correct spelling of model attribute

--- frontend/app/models/paper-trail-version.js
+++ frontend/app/models/paper-trail-version.js
@@ -2,7 +2,7 @@ import Model, { attr, belongsTo } from "@ember-data/model";
 
 export default class PaperTrailVersion extends Model {
   @attr("string") event;
-  @attr("number") user_id;
+  @attr("number") userId;
   @attr("string") username;
   @attr("string") createdAt;
   @attr() object;
