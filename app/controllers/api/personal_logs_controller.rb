fbbd205fbb08f4ae0b13d5e1dcf5559c0652168d
Julien Gaumez <julien.gaumez@students.unibe.ch>
2022-04-29 16:44:59 +0200

    some changes to the controller

--- app/controllers/api/personal_logs_controller.rb
+++ app/controllers/api/personal_logs_controller.rb
@@ -1,10 +1,11 @@
 # frozen_string_literal: true
 
 class Api::PersonalLogsController < ApiController
+    before_action :skip_authorization
+
     self.permitted_attrs = :whodunnit
   
     def index(options = {})
-      authorize(team, :index?, policy_class: LogPolicy)
       render({ json: fetch_entries,
                each_serializer: list_serializer,
                root: 'Logs_'.pluralize }
@@ -14,16 +15,12 @@ class Api::PersonalLogsController < ApiController
   
     def fetch_entries
       PaperTrail.serializer = JSON
-      logs = current_user.encryptables.all.versions.find_by!(whodunnit: params[:current_user.id])
-      logs.sort { |a, b| b.created_at <=> a.created_at }
+      logs = PaperTrail::Version.find_by!(whodunnit: current_user.id)
+      # logs.sort { |a, b| b.created_at <=> a.created_at }
     end
   
     def list_serializer
       @model_serializer ||= 'LogsSerializer'.constantize
-    end
-  
-    def team
-      @team ||= Encryptable.all.folder.team
     end
   end
   
