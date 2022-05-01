40616c4fb82817d9afe88897cd9d5684dde012d1
Renato Oester <renato.oester@bluewin.ch>
2022-05-01 20:48:55 +0200

    Revert " change a 'not' mistake"
    This reverts commit 81548f86f13e7e652b25f63ea80afb03e4482e47.

--- spec/system/personal_log_system_spec.rb
+++ spec/system/personal_log_system_spec.rb
@@ -9,7 +9,6 @@ it 'as admin is there everything avaible' do
     login_as_root
     visit('/log')
     expect(page).to have_text('Personal activity log') 
-    #no side bar
     expect(page).not_to have_text('button id="ember4608"')
     expect(page).to have_text('Action')
     expect(page).to have_text('Date')
@@ -22,7 +21,7 @@ it 'is the Log correct' do
 #action 
     expect(page).to have_text('these are not implemented yet') 
     #Date
-    expect(page).to have_text('this neither')
+    expect(page).not_to have_text('this neither')
     #Change
     expect(page).to have_text('nope just example')
     logout
@@ -34,8 +33,7 @@ it 'as user is there everything avaible' do
 
     expect(page).to have_text('Personal activity log') 
     expect(page).to have_text('Personal activity log') 
-    #no side bar
-    expect(page).not_to have_text('button id="ember4608"')
+    expect(page).to have_text('button id="ember4608"')
     expect(page).to have_text('Action')
     expect(page).to have_text('Date')
     expect(page).to have_text('Change')    
