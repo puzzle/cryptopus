263bb75786b17ef455008c701c217a4c334798e4
Niklas Jäggi <jaeggi@puzzle.ch>
2022-05-04 15:18:29 +0200

    fixed rebasing issues

--- frontend/app/models/team.js
+++ frontend/app/models/team.js
@@ -3,10 +3,14 @@ import Model, { attr, hasMany } from "@ember-data/model";
 export default class Team extends Model {
   @attr("string") name;
   @attr("string") description;
+  @attr("string") type;
   @attr("boolean") private;
   @attr("boolean") favourised;
   @attr("boolean") deletable;
-  @attr("boolean") personal_team;
   @hasMany("folder") folders;
   @hasMany("teammember") teammembers;
+
+  get isPersonalTeam() {
+    return this.type === "Team::Personal"
+  }
 }
