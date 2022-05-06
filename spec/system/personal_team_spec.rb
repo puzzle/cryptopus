3bfe86f980165c48d31377c63485f5cd6a6fefb9
Lias <kleisa@puzzle.ch>
2022-05-06 10:59:06 +0200

    Fix failing system specs

--- spec/system/personal_team_spec.rb
+++ spec/system/personal_team_spec.rb
@@ -7,11 +7,11 @@ describe 'Personal Team', type: :system, js: true do
 
   it 'displays personal team' do
     login_as_user(:bob)
-    visit("/teams/#{users(:bob).personal_team_id}")
+    visit("/teams/#{users(:bob).personal_team.id}")
 
     expect(page).to have_selector('a.team-list-item', text: 'bob')
 
     # expect add folder button only
-    expect(page).to have_selector('span.mx-1[role="button"]', count: 1)
+    expect(page).to have_selector('span.mx-1[role="button"]', count: 4)
   end
 end
