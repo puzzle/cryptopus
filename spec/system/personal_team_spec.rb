42679331e8232dc3805ac5c9e049c2a6640b50e6
Lias <kleisa@puzzle.ch>
2022-05-09 08:12:57 +0200

    Fix failing system spec (person_team_spec)

--- spec/system/personal_team_spec.rb
+++ spec/system/personal_team_spec.rb
@@ -9,9 +9,13 @@ describe 'Personal Team', type: :system, js: true do
     login_as_user(:bob)
     visit("/teams/#{users(:bob).personal_team.id}")
 
+    personal_team_header = find("div.row.py-2.d-flex.team-card-header.rounded-top")
+
     expect(page).to have_selector('a.team-list-item', text: 'bob')
 
-    # expect add folder button only
-    expect(page).to have_selector('span.mx-1[role="button"]', count: 4)
+    within(personal_team_header) do
+      # expect add folder button only
+      expect(page).to have_selector('span.mx-1[role="button"]', count: 1)
+    end
   end
 end
