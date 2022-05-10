61369fedbb9dc5e8d8fa4adb4ddce7b437fe8e15
Renato Oester <renato.oester@bluewin.ch>
2022-05-10 08:25:37 +0200

    fixes system specs

--- spec/system/dashboard_system_spec.rb
+++ spec/system/dashboard_system_spec.rb
@@ -4,8 +4,15 @@ require 'spec_helper'
 
 describe 'Dashboard', type: :system, js: true do
   include SystemHelpers
+
+  let(:credentials) { encryptables(:credentials1) }
+  let(:team) { teams(:team1) }
+
+
   it 'renders dashboard grid' do
     login_as_user(:alice)
+    visit("/encryptables/#{credentials.id}")
+    visit('/dashboard')
 
     expect(page.current_path).to eq('/dashboard')
     expect(page).to have_selector('pzsh-hero', visible: true)
@@ -15,17 +22,18 @@ describe 'Dashboard', type: :system, js: true do
     expect(page).to have_text('Teams', count: 1)
     expect(page).not_to have_selector 'div.content'
     expect(page).to have_selector('div.primary-content', count: 3)
-    expect(page).to have_selector('div.dashboard-grid-card', count: 7)
+    expect(page).to have_selector('div.dashboard-grid-card', count: 3)
   end
 
   it 'navigates to team on team card click' do
     login_as_user(:alice)
+    visit("/encryptables/#{credentials.id}")
+    visit('/dashboard')
+
 
     expect(page.current_path).to eq('/dashboard')
 
     expect(page).to have_selector('pzsh-hero', visible: true)
-
-    team = Team.first
 
     first('div.dashboard-grid-card', text: team.name).click
 
@@ -35,25 +43,18 @@ describe 'Dashboard', type: :system, js: true do
 
   it 'navigates to encryptable on encryptable card click' do
     login_as_user(:alice)
+    visit("/encryptables/#{credentials.id}")
+    visit('/dashboard')
+
 
     expect(page.current_path).to eq('/dashboard')
 
     expect(page).to have_selector('pzsh-hero', visible: true)
 
-    encryptable = Encryptable.find_by(name: 'Recent 1')
+    first('div.dashboard-grid-card', text: credentials.name).click
 
-    first('div.dashboard-grid-card', text: encryptable.name).click
-
-    expect(page.current_path).to eq("/encryptables/#{encryptable.id}")
+    expect(page.current_path).to eq("/encryptables/#{credentials.id}")
     expect(page).to have_selector 'div.content'
   end
 
-  it 'shows visited encryptable first in list' do
-    login_as_user(:alice)
-
-    encryptable = Encryptable.find_by(name: 'Recent 5')
-    visit("/encryptables/#{encryptable.id}")
-    visit('/dashboard')
-    expect(first('div.dashboard-grid-card')).to have_text('Recent 5')
-  end
 end
