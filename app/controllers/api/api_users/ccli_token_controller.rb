4d839a96acc505e3cadab66aa284a885905ead5e
renato-oester <91009092+renato-oester@users.noreply.github.com>
2022-05-24 10:24:00 +0200

    383 ccli login command button (#620)
    * add copyCcliLogin action to navbar
    
    * add necessary services to nav-bar.js and copy button to nav-bar template.
    
    * add ccli_token_controller.rb
    
    * create new api_user if no default_api_user set
    
    * Add icon and change name of Copy CCLI Login button
    
    * Change route to not have user_id
    
    * add ccli_token_controller_specs
    
    * add system specs for CCLI login copy
    
    * fix rubocop errors
    
    Co-authored-by: Julien Gaumez <julien.gaumez@students.unibe.ch>

---
+++ app/controllers/api/api_users/ccli_token_controller.rb
@@ -0,0 +1,39 @@
+# frozen_string_literal: true
+
+require 'user/api'
+
+class Api::ApiUsers::CcliTokenController < ApiController
+
+  # GET /api/api_users/:userId/ccli_token
+  def show
+    authorize api_user
+    renewed_token = renew_token
+    render_json(info: [t('flashes.api.api-users.token.renew',
+                         username: api_user.username, token: renewed_token)],
+                errors: [],
+                token: renewed_token,
+                username: api_user.username)
+  end
+
+  private
+
+  def api_user
+    @api_user ||= default_api_user || current_user.api_users.create
+  end
+
+  def default_api_user
+    @default_api_user ||= User::Api.find_by id: current_user.default_ccli_user_id
+  end
+
+  def renew_token
+    if active_session? # api users can't have sessions
+      private_key = session[:private_key]
+      return api_user.renew_token_by_human(private_key)
+    end
+
+    if current_user.human?
+      private_key = current_user.decrypt_private_key(password_header)
+      api_user.renew_token_by_human(private_key)
+    end
+  end
+end
