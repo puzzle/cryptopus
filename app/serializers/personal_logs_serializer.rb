a3f43f4c8829fd9ff8b1edd18b90827e1d95e716
Julien Gaumez <julien.gaumez@students.unibe.ch>
2022-05-24 16:22:21 +0200

    Revert "don't serialize encryptable_name and _id but only use it through belongs_to association"
    This reverts commit 0166d18f0c3e831cefef5a1c72b47cbefef6685a.

--- app/serializers/personal_logs_serializer.rb
+++ app/serializers/personal_logs_serializer.rb
@@ -1,7 +1,8 @@
 # frozen_string_literal: true
 
 class PersonalLogsSerializer < ApplicationSerializer
-  attributes :id, :event, :user_id, :username, :created_at, :folder_name, :team_name
+  attributes :id, :event, :user_id, :username, :created_at,
+             :encryptable_name, :folder_name, :team_name, :encryptable_id
 
   belongs_to :encryptable, serializer: EncryptableMinimalSerializer
 
@@ -13,11 +14,19 @@ class PersonalLogsSerializer < ApplicationSerializer
     object.user.username
   end
 
+  def encryptable_name
+    object.encryptable.name
+  end
+
   def folder_name
     object.encryptable.folder.name
   end
 
   def team_name
     object.encryptable.folder.team.name
+  end
+
+  def encryptable_id
+    object.encryptable.id
   end
 end
