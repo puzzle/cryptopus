891810e95167577678e2274cd5ac640686776241
Lias <kleisa@puzzle.ch>
2022-06-03 10:13:21 +0200

    Implement user_candidates_controller

--- app/controllers/api/user_candidates_controller.rb
+++ app/controllers/api/user_candidates_controller.rb
@@ -1,9 +1,26 @@
 # frozen_string_literal: true
 
-class Api::UserCandidatesController < ApplicationController
+class Api::UserCandidatesController < ApiController
   def index
-    authorize team, :team_member?
-    candidates = team.member_candidates
-    render_json candidates
+    if team_id_present?
+      authorize team, :team_member?
+      candidates = team.member_candidates
+      render_json candidates
+    else
+      authorize :user_candidates, :index?
+      candidates = sharing_candidates
+      render_json candidates
+    end
   end
+
+  private
+
+  def team_id_present?
+    params[:team_id].present?
+  end
+
+  def sharing_candidates
+    User::Human.where.not(id: current_user.id)
+  end
+
 end
--- app/policies/user_candidates_policy.rb
+++ app/policies/user_candidates_policy.rb
@@ -1,1 +1,7 @@
+# frozen_string_literal: true
 
+class UserCandidatesPolicy < ApplicationPolicy
+  def index?
+    true
+  end
+end
