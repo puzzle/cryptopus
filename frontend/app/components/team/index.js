2738294b1c5ab5bc27474054e81b5107e9b9081e
Luca Brügger <lbruegger@puzzle.ch>
2022-06-23 12:08:30 +0200

    Fix spinner

---
+++ frontend/app/components/team/index.js
@@ -0,0 +1,16 @@
+import Component from "@glimmer/component";
+import { action } from "@ember/object";
+import { inject as service } from "@ember/service";
+import { tracked } from "@glimmer/tracking";
+
+export default class Index extends Component {
+  @service navService;
+  @service loading;
+
+  @tracked model = this.args.model;
+  @tracked q = this.args.q;
+
+  constructor() {
+    super(...arguments);
+  }
+}
