26d7c9ee59057213759f28d5e12bafa849457629
Niklas Jäggi <jaeggi@puzzle.ch>
2022-06-27 14:26:22 +0200

    remove policy changes from branch

--- app/policies/encryptable_policy.rb
+++ app/policies/encryptable_policy.rb
@@ -15,7 +15,7 @@ class EncryptablePolicy < TeamDependantPolicy
   end
 
   def create?
-    team.teammember?(@user.id) || transferring_encryptable?
+    team.teammember?(@user.id)
   end
 
   def update?
@@ -31,22 +31,4 @@ class EncryptablePolicy < TeamDependantPolicy
   def team
     @record.team
   end
-
-  private
-
-  def transferring_encryptable?
-    @record.receiver_id.present? &&
-      receiver_personal_team_owner? &&
-      user_human? &&
-      @record.is_a?(Encryptable::File)
-  end
-
-  def user_human?
-    User.find(@record.receiver_id).is_a?(User::Human)
-  end
-
-  def receiver_personal_team_owner?
-    @record.folder.team.owner.id == @record.receiver_id
-  end
-
 end
