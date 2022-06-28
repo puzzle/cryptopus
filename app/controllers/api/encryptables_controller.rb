f9feab45340d128e7ea044f1ae62753c32b9e400
Niklas Jäggi <jaeggi@puzzle.ch>
2022-06-28 08:13:34 +0200

    remove changes for encrytbale controller from branch

--- app/controllers/api/encryptables_controller.rb
+++ app/controllers/api/encryptables_controller.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 class Api::EncryptablesController < ApiController
+  include Encryptables
 
   self.permitted_attrs = [:name, :description, :tag]
 
@@ -38,13 +39,13 @@ class Api::EncryptablesController < ApiController
 
   # PATCH /api/encryptables/:id?Query
   def update
-    authorize entry
-    entry.attributes = model_params
+    authorize encryptable
+    encryptable.attributes = model_params
 
-    encrypt(entry)
+    encrypt(encryptable)
 
-    if entry.save
-      render_json entry
+    if encryptable.save
+      render_json encryptable
     else
       render_errors
     end
@@ -58,8 +59,8 @@ class Api::EncryptablesController < ApiController
       Encryptable::OseSecret
     elsif action_name == 'destroy'
       Encryptable
-    elsif entry.present?
-      entry.class
+    elsif @encryptable.present?
+      encryptable.class
     elsif credential_id.present?
       Encryptable::File
     else
@@ -78,12 +79,8 @@ class Api::EncryptablesController < ApiController
     Encryptable::Credentials.find(credential_id)
   end
 
-  def entry
-    @encryptable ||= Encryptable.find(entry_id)
-  end
-
-  def entry_id
-    params[:id] || params.dig('data', 'attributes', 'id')
+  def encryptable
+    @encryptable ||= Encryptable.find(params[:id])
   end
 
   def encryptable_file?
@@ -107,7 +104,7 @@ class Api::EncryptablesController < ApiController
   end
 
   def encryptable_move_handler
-    EncryptableMoveHandler.new(entry, session[:private_key], current_user)
+    EncryptableMoveHandler.new(encryptable, session[:private_key], current_user)
   end
 
   def ivar_name
@@ -130,76 +127,5 @@ class Api::EncryptablesController < ApiController
     else
       []
     end
-  end
-
-  ### Entries ###
-
-  def fetch_entries
-    return fetch_encryptable_files if credential_id.present?
-
-    encryptables = user_encryptables
-    if tag_param.present?
-      encryptables = encryptables.find_by(tag: tag_param)
-    end
-    encryptables
-  end
-
-  def render_entry(options = nil)
-    return send_file(options) if encryptable_file? && action_name != 'create'
-
-    super(options)
-  end
-
-  ### Files ###
-
-  def send_file(options)
-    send_data(entry.cleartext_file, { filename: entry.name,
-                                      type: entry.content_type,
-                                      disposition: 'attachment' }
-                                      .merge(options || {}))
-  end
-
-  def fetch_encryptable_files
-    Encryptable::File.where(credential_id: user_encryptables.pluck(:id))
-                     .where(credential_id: credential_id)
-  end
-
-  def build_encryptable_file
-    filename = params[:file].original_filename
-    file = new_file(file_credential, params[:description], filename)
-    file.content_type = params[:file].content_type
-    file.cleartext_file = params[:file].read
-
-    instance_variable_set(:"@#{ivar_name}", file)
-  end
-
-  def new_file(parent_encryptable, description, name)
-    Encryptable::File.new(encryptable_credential: parent_encryptable,
-                          description: description,
-                          name: name)
-  end
-
-  def credential_id
-    return entry.credential_id if params[:id].present?
-
-    params[:credential_id]
-  end
-
-  ### Other ###
-
-  def encrypt(encryptable)
-    if encryptable.folder_id_changed?
-      # if folder id changed recheck team permission
-      authorize encryptable
-      # move handler calls encrypt implicit
-      encryptable_move_handler.move
-    else
-      encryptable.encrypt(decrypted_team_password(team))
-    end
-  end
-
-  def create_ose_secret?
-    action_name == 'create' &&
-      params.dig('data', 'attributes', 'type') == 'ose_secret'
   end
 end
