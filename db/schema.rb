c7f7e39b1248a1d68e58b04d982854e51c166beb
Luca Brügger <lbruegger@puzzle.ch>
2022-06-28 16:14:25 +0200

    UI update

--- db/schema.rb
+++ db/schema.rb
@@ -22,6 +22,8 @@ ActiveRecord::Schema[7.0].define(version: 2022_06_09_063817) do
     t.text "encrypted_data", limit: 16777215
     t.integer "credential_id"
     t.text "content_type"
+    t.binary "encrypted_transfer_password"
+    t.integer "sender_id"
     t.index ["description"], name: "index_encryptables_on_description"
     t.index ["name"], name: "index_encryptables_on_name"
     t.index ["tag"], name: "index_encryptables_on_tag"
