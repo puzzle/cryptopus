51d7be63a34fa0a1e0fbdbe93ace482ac4201504
Lias <kleisa@puzzle.ch>
2022-07-14 15:55:13 +0200

    Remove pry

--- app/policies/encryptable_transfer_policy.rb
+++ app/policies/encryptable_transfer_policy.rb
@@ -3,9 +3,7 @@
 class EncryptableTransferPolicy < TeamDependantPolicy
 
    def create?
-     require 'pry'; binding.pry unless $pstop
-     true
-     #current_user.present?
+      current_user.present?
    end
 
 end
