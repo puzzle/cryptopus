9cce367468165a9c1fcc3de1751eed0d9cb9438b
Lias Kleisa <kleisa@puzzle.ch>
2023-04-28 16:18:29 +0200

    Frontend add unread transferred files to team model and display batch on personal team

--- app/serializers/team_list_serializer.rb
+++ app/serializers/team_list_serializer.rb
@@ -12,8 +12,8 @@ class TeamListSerializer < ApplicationSerializer
   end
 
   def unread_transferred_files
-      # rubocop:enable Metrics/LineLength
-      object.personal_team? ? Folder.find(object.folders.where(name: "inbox").first.id).encryptables.all.where.not(encrypted_transfer_password: nil).count : 0
-      # rubocop:disable Metrics/LineLength
+    # rubocop:disable Metrics/LineLength
+    object.personal_team? ? Folder.find(object.folders.where(name: 'inbox').first.id).encryptables.all.where.not(encrypted_transfer_password: nil).count : 0
+    # rubocop:enable Metrics/LineLength
   end
 end
