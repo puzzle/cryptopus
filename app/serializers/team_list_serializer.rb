1aad56c7043a25acb0f8b9069c8d793f7d80d012
Lias Kleisa <kleisa@puzzle.ch>
2023-04-28 14:08:21 +0200

    Return number of unread transferred files in folder serializer

--- app/serializers/team_list_serializer.rb
+++ app/serializers/team_list_serializer.rb
@@ -3,11 +3,17 @@
 class TeamListSerializer < ApplicationSerializer
   # To hide STI name in Frontend
   type Team.name.pluralize
-  attributes :id, :name, :type, :personal_team, :private
+  attributes :id, :name, :type, :personal_team, :private, :unread_transferred_files
 
   has_many :folders, serializer: FolderMinimalSerializer
 
   def personal_team
     object.personal_team?
+  end
+
+  def unread_transferred_files
+      # rubocop:enable Metrics/LineLength
+      object.personal_team? ? Folder.find(object.folders.where(name: "inbox").first.id).encryptables.all.where.not(encrypted_transfer_password: nil).count : 0
+      # rubocop:disable Metrics/LineLength
   end
 end
