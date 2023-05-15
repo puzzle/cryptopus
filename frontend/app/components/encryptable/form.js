fcc68deaa1a46899b326b91bc95967f89eced9cf
Niklas Jäggi <jaeggi@puzzle.ch>
2023-05-15 14:00:57 +0200

    fix prettier

--- frontend/app/components/encryptable/form.js
+++ frontend/app/components/encryptable/form.js
@@ -226,7 +226,7 @@ export default class Form extends BaseFormComponent {
   handleSubmitError(response) {
     if (response.errors.length > 0) {
       //get error message to show correct error in form
-      this.Errors = response.errors[0].detail
+      this.Errors = response.errors[0].detail;
     }
   }
 
