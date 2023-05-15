f0a44cf4727cd612bc1cac6b1751a980b42b5663
Niklas Jäggi <jaeggi@puzzle.ch>
2023-05-15 13:03:28 +0200

    replace single quotation marks with double in validation files

--- frontend/app/validations/encryptable.js
+++ frontend/app/validations/encryptable.js
@@ -7,33 +7,33 @@ import {
 export default {
   name: [validatePresence(true), validateLength({ max: 70 })],
   cleartextPassword: [
-    validateExclusion({ list: [''] }),
+    validateExclusion({ list: [""] }),
     validateLength({ max: 70 })
   ],
   cleartextUsername: [
-    validateExclusion({ list: [''] }),
+    validateExclusion({ list: [""] }),
     validateLength({ max: 70 })
   ],
   cleartextToken: [
-    validateExclusion({ list: [''] }),
+    validateExclusion({ list: [""] }),
     validateLength({ max: 70 })
   ],
   cleartextPin: [
-    validateExclusion({ list: [''] }),
+    validateExclusion({ list: [""] }),
     validateLength({ max: 70 })
   ],
   cleartextEmail: [
-    validateExclusion({ list: [''] }),
+    validateExclusion({ list: [""] }),
     validateLength({ max: 70 })
   ],
   label: [
-    validatePresence({presence: true, on: 'value'}),
-    validateExclusion({ list: [''] }),
+    validatePresence({presence: true, on: "value"}),
+    validateExclusion({ list: [""] }),
     validateLength({ max: 70 })
   ],
   value: [
-    validatePresence({presence: true, on: 'label'}),
-    validateExclusion({ list: [''] }),
+    validatePresence({presence: true, on: "label"}),
+    validateExclusion({ list: [""] }),
     validateLength({ max: 70 })
   ],
   description: [validateLength({ max: 4000 })],
