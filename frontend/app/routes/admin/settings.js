0a6617101411b19fe475836498f271e4c7229fec
Yanick Minder <minder@puzzle.ch>
2023-06-05 15:59:38 +0200

    works local but not in docker

--- frontend/app/routes/admin/settings.js
+++ frontend/app/routes/admin/settings.js
@@ -1,5 +1,5 @@
 import AdminRoute from "../admin";
-import { getNames } from "ember-i18n-iso-countries";
+import { getNames } from "i18n-iso-countries";
 import RSVP from "rsvp";
 import ENV from "../../config/environment";
 
@@ -11,6 +11,7 @@ export default class AdminSettingsRoute extends AdminRoute {
     } else {
       locale = "de";
     }
+    console.log(getNames(locale));
     const ISOArray = Object.entries(getNames(locale));
     const countries = ISOArray.map((countryArray) => {
       return { value: countryArray[0], label: countryArray[1] };
