ea7f97b76df955e96978139340cf72d92d105cc7
Niklas Jäggi <jaeggi@puzzle.ch>
2023-06-12 10:20:41 +0200

    rebase onto new presence check

--- app/models/encryptable.rb
+++ app/models/encryptable.rb
@@ -74,16 +74,10 @@ class Encryptable < ApplicationRecord
   def encrypt_attr(attr, team_password)
     cleartext_value = send(:"cleartext_#{attr}")
 
-    # Rubocop suggests correctable from .nil? || .empty? to .blank?
-    # Rubocop is here disabled, because .blank? is not working on UTF-8 String which are not ASCII
-    # Rubocop correctable must be disabled with all
-    # rubocop:disable all
-    encrypted_value = if cleartext_value.nil? || cleartext_value.empty?
-                        nil
-                      else
-                        Crypto::Symmetric::Aes256.encrypt(cleartext_value, team_password)
-                      end
-    # rubocop:enable all
+    encrypted_value =
+      cleartext_value.presence &&
+        Crypto::Symmetric::Aes256.encrypt(cleartext_value, team_password)
+
     build_encrypted_data(attr, encrypted_value)
   end
 
