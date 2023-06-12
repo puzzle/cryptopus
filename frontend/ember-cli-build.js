2288064e5edede358fb1afdb0303fa1324b40206
Yanick Minder <minder@puzzle.ch>
2023-06-12 10:10:29 +0200

    remove deprecations

--- frontend/ember-cli-build.js
+++ frontend/ember-cli-build.js
@@ -23,7 +23,7 @@ module.exports = function (defaults) {
     "ember-bootstrap": {
       bootstrapVersion: 4,
       importBootstrapCSS: true,
-      importBootstrapFont: true
+      importBootstrapFont: false
     },
     intl: {
       silent: true
