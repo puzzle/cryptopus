d595fe3bd14fba0bc5988359c260e7499de4dc7e
Yanick Minder <minder@puzzle.ch>
2023-06-14 14:35:13 +0200

    start using marked

--- frontend/app/components/changelog.js
+++ frontend/app/components/changelog.js
@@ -1,3 +1,22 @@
 import Component from "@glimmer/component";
+import { marked } from "marked";
 
-export default class Changelog extends Component {}
+export default class Changelog extends Component {
+  text = marked.parse("## This is a test string");
+
+  constructor() {
+    super(...arguments);
+    const req = new XMLHttpRequest();
+    req.open(
+      "GET",
+      "https://raw.githubusercontent.com/puzzle/cryptopus/master/frontend/markdown/CHANGELOG.md",
+      false
+    );
+    req.addEventListener("load", this.reqListener);
+    req.send();
+  }
+
+  reqListener() {
+    this.text = marked.parse(this.reponseText);
+  }
+}
