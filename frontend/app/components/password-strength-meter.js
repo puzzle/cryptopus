b11ac0a1a4cce50e334b3cafd4dc4b7fc53e7d94
Niklas Jäggi <jaeggi@puzzle.ch>
2023-06-16 15:07:04 +0200

    downgrade bootstrap to fix styling and remove passwordStrength service and replace it with new method

--- frontend/app/components/password-strength-meter.js
+++ frontend/app/components/password-strength-meter.js
@@ -1,28 +1,28 @@
-import { inject as service } from "@ember/service";
 import Component from "@ember/component";
 import { tracked } from "@glimmer/tracking";
 import { isPresent } from "@ember/utils";
+import zxcvbn from "zxcvbn";
 
 export default class PasswordStrengthMeterComponent extends Component {
-  @service passwordStrength;
-
   @tracked barWidth = 0;
   @tracked score = 0;
 
   constructor() {
     super(...arguments);
+  }
 
-    this.passwordStrength.load();
+  get passwordStrengthScore() {
+    const { score } = zxcvbn(this.password);
+
+    return score;
   }
 
   didReceiveAttrs() {
     super.didReceiveAttrs();
     if (isPresent(this.password) && this.password !== "") {
-      this.passwordStrength.strength(this.password).then((strength) => {
-        this.score = strength.score;
-        if (this.score === 0) this.score = 1;
-        this.barWidth = this.score * 25;
-      });
+      this.score = this.passwordStrengthScore;
+      if (this.score === 0) this.score = 1;
+      this.barWidth = this.score * 25;
     } else {
       this.score = 0;
       this.barWidth = 0;
