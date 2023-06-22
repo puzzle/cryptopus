e1058dacadbd51a166366681a28ae22dd99c67b5
Yanick Minder <minder@puzzle.ch>
2023-06-22 09:06:16 +0200

    remove babel include polyfill

--- frontend/ember-cli-build.js
+++ frontend/ember-cli-build.js
@@ -5,9 +5,6 @@ const EmberApp = require("ember-cli/lib/broccoli/ember-app");
 module.exports = function (defaults) {
   let app = new EmberApp(defaults, {
     storeConfigInMeta: false,
-    babel: {
-      includePolyfill: true
-    },
     "ember-cli-babel": {
       includePolyfill: true
     },
