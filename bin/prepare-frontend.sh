b5d028e595912ee4742ba9320f6ace4b27b93dc1
Mountain Star <github@mtnstar.net>
2023-06-28 13:14:31 +0200

    Switch from yarn to npm

--- bin/prepare-frontend.sh
+++ bin/prepare-frontend.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-export NODE_OPTIONS=--openssl-legacy-provider && yarn --cwd frontend/ install
-export NODE_OPTIONS=--openssl-legacy-provider && yarn --cwd frontend/ build-prod
+export NODE_OPTIONS=--openssl-legacy-provider && npm install --prefix frontend/
+export NODE_OPTIONS=--openssl-legacy-provider && npm run build-prod --prefix frontend/
 rm public/frontend-index.html
 rsync --ignore-existing -r frontend/dist/* public/
 mv public/{index,frontend-index}.html
