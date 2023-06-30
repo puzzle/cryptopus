8d445aad23454a9c8847d5884a616085a84fbd20
Lias Kleisa <kleisa@puzzle.ch>
2023-06-30 14:28:14 +0200

    Add new credential fabricators for recrypt spec

--- spec/fabricators/encryptables/credentials_fabricator.rb
+++ spec/fabricators/encryptables/credentials_fabricator.rb
@@ -5,11 +5,33 @@
 #  See the COPYING file at the top-level directory or at
 #  https://github.com/puzzle/cryptopus.
 
-Fabricator(:credential, from: 'Encryptable::Credentials') do
+Fabricator(:credential_all_attrs, from: 'Encryptable::Credentials') do
   transient :team_password
   name { Faker::Team.creature }
   cleartext_username { Faker::Internet.user_name }
   cleartext_password { Faker::Internet.password }
+  cleartext_token { Faker::Internet.uuid }
+  cleartext_pin { Faker::Internet.ip_v4_address }
+  cleartext_email { Faker::Internet.email }
+  cleartext_custom_attr_label { Faker::Internet.user_name }
+  cleartext_custom_attr { Faker::Internet.password }
+  before_create do |encryptable, attrs|
+    encryptable.encrypt(attrs[:team_password])
+  end
+end
+
+Fabricator(:credential_one_attr, from: 'Encryptable::Credentials') do
+  transient :team_password
+  name { Faker::Team.creature }
+  cleartext_token { Faker::Internet.uuid }
+  before_create do |encryptable, attrs|
+    encryptable.encrypt(attrs[:team_password])
+  end
+end
+
+Fabricator(:credential_no_attr, from: 'Encryptable::Credentials') do
+  transient :team_password
+  name { Faker::Team.creature }
   before_create do |encryptable, attrs|
     encryptable.encrypt(attrs[:team_password])
   end
