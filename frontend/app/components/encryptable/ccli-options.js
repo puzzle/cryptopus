bec028c3ecfd4d308036a27fa627f87b37893bcb
Niklas Jäggi <jaeggi@puzzle.ch>
2023-07-03 16:48:31 +0200

    introduce other attributes from custom attrs

--- frontend/app/components/encryptable/ccli-options.js
+++ frontend/app/components/encryptable/ccli-options.js
@@ -1,21 +1,8 @@
 import { action } from "@ember/object";
-import AccountValidations from "../../validations/encryptable";
-import lookupValidator from "ember-changeset-validations";
-import Changeset from "ember-changeset";
 import { inject as service } from "@ember/service";
-import { tracked } from "@glimmer/tracking";
-import BaseFormComponent from "../base-form-component";
-import { isPresent } from "@ember/utils";
-import { isEmpty } from "@ember/utils";
-import { capitalize } from "@ember/string";
-import { A } from "@ember/array";
 import Component from "@glimmer/component";
 
 export default class Form extends Component {
-  @service store;
-  @service router;
-  @service navService;
-  @service userService;
   @service notify;
   @service clipboardService;
   @service intl;
@@ -34,8 +21,6 @@ export default class Form extends Component {
   @action
   copyCommand(command) {
     this.clipboardService.copy(command);
-    this.notify.info(
-      this.intl.t(`flashes.encryptables.ccli_command_copied`)
-    );
+    this.notify.info(this.intl.t(`flashes.encryptables.ccli_command_copied`));
   }
 }
