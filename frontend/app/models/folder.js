a3a314fcac1832167f9e7fe491fbd77aa6e40ba7
megli2 <egli@puzzle.ch>
2023-07-07 15:24:16 +0200

    upgrade first half of packages which don't cause frontend to fail

--- frontend/app/models/folder.js
+++ frontend/app/models/folder.js
@@ -4,8 +4,8 @@ export default class Folder extends Model {
   @attr("string") name;
   @attr("string") description;
   @attr("number") unreadTransferredCount;
-  @hasMany("encryptable", { async: true , inverse: "folder" }) encryptables;
-  @belongsTo("team", { async: true , inverse: "folders" }) team;
+  @hasMany("encryptable", { async: true, inverse: "folder" }) encryptables;
+  @belongsTo("team", { async: true, inverse: "folders" }) team;
 
   get isInboxFolder() {
     return this.name === "inbox" && this.team.get("isPersonalTeam");
