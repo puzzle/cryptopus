8af27686f01ec1cf9ea934c551c530ed3f83224d
megli2 <egli@puzzle.ch>
2023-07-10 11:02:03 +0200

    remove deprecation by using 'fetch' instead of XMLHttpRequest and move setting of ENVs to application route

--- frontend/app/routes/application.js
+++ frontend/app/routes/application.js
@@ -7,7 +7,10 @@ export default class IndexRoute extends Route {
   @service notify;
   @service intl;
 
-  beforeModel() {
+  async beforeModel() {
+    const response = await fetch("/api/env_settings");
+    const envSettings = await response.json();
+    this.setEnvSettings(envSettings);
     let selectedLocale = this.intl.locales.includes(
       ENV.preferredLocale.replace("_", "-")
     )
@@ -15,6 +18,26 @@ export default class IndexRoute extends Route {
       : "en";
 
     this.intl.setLocale(selectedLocale);
+  }
+
+  setEnvSettings(envSettings) {
+    ENV.sentryDsn = envSettings.sentry;
+    ENV.currentUserId = envSettings.current_user.id;
+    ENV.currentUserRole = envSettings.current_user.role;
+    ENV.currentUserGivenname = envSettings.current_user.givenname;
+    ENV.currentUserUsername = envSettings.current_user.username;
+    ENV.currentUserLastLoginAt = envSettings.current_user.last_login_at;
+    ENV.currentUserLastLoginFrom = envSettings.current_user.last_login_from;
+    ENV.currentUserAuth = envSettings.current_user.auth;
+    ENV.currentUserDefaultCcliUserId =
+      envSettings.current_user.default_ccli_user_id;
+    ENV.preferredLocale = envSettings.current_user.preferred_locale;
+    ENV.lastLoginMessage = envSettings.last_login_message;
+    ENV.geoIP = envSettings.geo_ip;
+    ENV.appVersion = envSettings.version;
+    ENV.CSRFToken = envSettings.csrf_token;
+    ENV.authProvider = envSettings.auth_provider;
+    ENV.fallbackInfo = envSettings.fallback_info;
   }
 
   @action
