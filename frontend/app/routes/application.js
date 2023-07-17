330039d0d02a83b43095459574c1e977c4aa776b
megli2 <egli@puzzle.ch>
2023-07-17 10:29:37 +0200

    fix system spec by moving operations into async beforeModel hook

--- frontend/app/routes/application.js
+++ frontend/app/routes/application.js
@@ -3,7 +3,7 @@ import { inject as service } from "@ember/service";
 import { action } from "@ember/object";
 import ENV from "../config/environment";
 
-export default class IndexRoute extends Route {
+export default class ApplicationRoute extends Route {
   @service notify;
   @service intl;
 
