9759b4ff62451e3b5b7daca6ad5fd14a7fcf88f4
megli2 <egli@puzzle.ch>
2023-07-21 13:02:31 +0200

    clean up classes to match rubocop claims

--- app/presenters/teams/pg_searching.rb
+++ app/presenters/teams/pg_searching.rb
@@ -10,13 +10,16 @@ module ::Teams
       # Get matching ids with pg_search
       matching_team_ids = teams.search_by_name(query).pluck :id
       matching_folder_ids = Folder.where({ id: allowed_folders }).search_by_name(query).pluck :id
-      matching_encryptable_ids = Encryptable.where({ id: allowed_encryptables }).search_by_name(query).pluck :id
+      matching_encryptable_ids = Encryptable.where({ id: allowed_encryptables })
+                                            .search_by_name(query).pluck :id
 
       # Join allowed tables together and return matching ones
       teams.where('encryptables.id IN (:encryptable_ids) OR teams.id IN (:team_ids) OR folders.id IN (:folder_ids)',
-                           encryptable_ids: matching_encryptable_ids, team_ids: matching_team_ids, folder_ids: matching_folder_ids)
-                    .references(:folders,
-                                folders: [:encryptables])
+                  encryptable_ids: matching_encryptable_ids,
+                  team_ids: matching_team_ids,
+                  folder_ids: matching_folder_ids)
+           .references(:folders,
+                       folders: [:encryptables])
     end
   end
 end
