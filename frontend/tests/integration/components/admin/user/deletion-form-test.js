0bb9b18101db886f3803a7c138b0bb6f9a9fcd3d
Yanick Minder <minder@puzzle.ch>
2024-05-02 09:41:28 +0200

    create a store stub for integration test

--- frontend/tests/integration/components/admin/user/deletion-form-test.js
+++ frontend/tests/integration/components/admin/user/deletion-form-test.js
@@ -1,10 +1,28 @@
 import { module, test } from "qunit";
 import { setupRenderingTest } from "ember-qunit";
-import { render } from "@ember/test-helpers";
+import { render, pauseTest } from "@ember/test-helpers";
 import { hbs } from "ember-cli-htmlbars";
+
+
+const storeStub = Service.extend({
+  query(modelName, params) {
+    if (params) {
+      return Promise.all([
+        { name: "Team1", description: "description1" },
+        { name: "Team2", description: "description2" },
+      ]);
+    }
+  }
+});
 
 module("Integration | Component | admin/user/deletion-form", function (hooks) {
   setupRenderingTest(hooks);
+
+
+  hooks.beforeEach(function () {
+    this.owner.unregister("service:store");
+    this.owner.register("service:store", storeStub);
+  });
 
   test("it renders with block", async function (assert) {
     await render(hbs`
@@ -12,6 +30,20 @@ module("Integration | Component | admin/user/deletion-form", function (hooks) {
         Delete
       </Admin::User::DeletionForm>
     `);
+
+    assert.equal(this.element.textContent.trim(), "Delete");
+  });
+
+  test("it renders with block", async function (assert) {
+    this.set("user", {
+      id: 12,
+      givenname: "Bob",
+      surname: "Muster",
+      username: "bob"
+    });
+
+    await render(hbs `<Admin::User::DeletionForm @user={{this.user}}/>`);
+    await pauseTest();
 
     assert.equal(this.element.textContent.trim(), "Delete");
   });
